846401f74c94c9973e484359cd94d6a16912d285
Joseph C Wang <joequant@gmail.com>
2013-12-04 22:00:25 +0800

    fix nightly

--- cron/nightly.sh
+++ cron/nightly.sh
@@ -3,13 +3,14 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $SCRIPT_DIR
 pushd ../../OG-Platform
 pushd examples/examples-simulated
-mvn opengamma:server-stop -Dconfig=fullstack
+mvn opengamma:server-stop -Dconfig=fullstack 
 popd
 git fetch upstream
 git checkout develop
 git rebase upstream/develop
 git checkout asia-fixes
-mvn install 
+git rebase upstream/develop
+mvn install -DskipTests
 pushd examples/examples-simulated
 mvn opengamma:server-start -Dconfig=fullstack
 popd
