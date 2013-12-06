913ffbe23bbcc3c12b3ee991e10322b8863826a2
Joseph C Wang <joequant@gmail.com>
2013-12-06 19:26:41 +0800

    add rpm deps

---
+++ rpm/install-deps.sh
@@ -0,0 +1,18 @@
+sudo urpmi \
+maven \
+maven-clean-plugin \
+maven-assembly-plugin \
+maven-compiler-plugin \
+maven-dependency-plugin \
+maven-install-plugin \
+maven-release-plugin \
+aether \
+aether-connector-basic \
+aether-transport-file \
+aether-transport-http \
+aether-transport-wagon \
+fop 
+
+
+
+
