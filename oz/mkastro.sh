1fa44ef4e549593e2d3db38990bca73ca487f4df
Joseph C Wang <joequant@gmail.com>
2013-12-23 02:15:23 +0800

    adadd image creation scripts

---
+++ oz/mkastro.sh
@@ -0,0 +1,1 @@
+oz-install -d3 -t 7200 -c oz-qcow2.cfg -a MageiaAstro.auto mageiaastroiso.tdl
---
+++ oz/mkimg.sh
@@ -0,0 +1,2 @@
+rm ~/.oz/images/Mageia.qcow2
+oz-install -a examples/MageiaStack.auto mageiaiso.tdl -c oz-qcow2.cfg  -d3 -t 3600
