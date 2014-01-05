7b56496bab677c846b1575dfbf87dc4b4ab4075e
Joseph C Wang <joequant@gmail.com>
2014-01-05 16:09:51 +0800

    add r build

--- cron/nightly.sh
+++ cron/nightly.sh
@@ -2,23 +2,16 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $SCRIPT_DIR
 pushd ../../OG-Platform
-git stash
-git checkout asia-fixes
 pushd examples/examples-simulated
 mvn opengamma:server-stop -Dconfig=fullstack 
 popd
-git stash
-git fetch upstream
-git fetch origin
-git checkout develop
-git rebase origin/develop
-git rebase upstream/develop
-git checkout asia-fixes
-git rebase origin/asia-fixes
-git rebase upstream/develop
-mvn install -DskipTests
+mvn install -Dmaven.test.skip=True
 pushd examples/examples-simulated
 mvn opengamma:server-init -Dconfig=fullstack
 mvn opengamma:server-start -Dconfig=fullstack
 popd
+popd
+
+pushd ../../OG-PlatformNative
+mvn install -Dmaven.test.skip=True
 popd
