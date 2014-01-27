6663deb95226e3744f5313b3e8e9fbba5274f8a7
Joseph C Wang <joequant@gmail.com>
2014-01-27 13:41:55 +0800

    set up flask web site

---
+++ web/bitquant/bitquant.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+from flask import Flask, request
+app = Flask(__name__, static_url_path='', static_folder='')
+
+@app.route("/")
+def root():
+    return app.send_static_file('index.html')
+
+@app.route("/test")
+def test():
+    return "test"
+
+if __name__ == "__main__":
+    app.run()
---
+++ web/bitquant/bitquant.wsgi
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+import sys
+import os
+sys.path.insert(0, os.path.dirname(os.path.realpath(__file__)))
+
+from bitquant import app as application
