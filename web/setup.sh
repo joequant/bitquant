bd394215b15e2e6b7280403cd0bc8081ee364ebd
Joseph C Wang <joequant@gmail.com>
2014-02-08 02:08:30 +0800

    remove og items from setup

--- web/setup.sh
+++ web/setup.sh
@@ -4,19 +4,12 @@ pushd /var/www/html
 rm -f *
 for i in $SCRIPT_DIR/bitquant/*; do 
 echo $(basename $i)
-sudo ln -s -f  ../../..$SCRIPT_DIR/bitquant/$(basename $i) $(basename $i)
+ln -s -f  ../../..$SCRIPT_DIR/bitquant/$(basename $i) $(basename $i)
 done
 popd
 
 pushd /etc/httpd/conf/webapps.d
-sudo mv -f 00_default_vhosts.conf 00_default_vhosts.conf.bak
-sudo ln -s -f ../../../..$SCRIPT_DIR/00_bitquant.conf 00_bitquant.conf
+mv -f 00_default_vhosts.conf 00_default_vhosts.conf.bak
+ln -s -f ../../../..$SCRIPT_DIR/00_bitquant.conf 00_bitquant.conf
 popd
-
-if [ -d ../../OG-Platform ] ; then
-pushd ../../OG-Platform/examples/examples-bitquant/
-mvn install
-mvn opengamma:server-init -Dconfig=fullstack
-popd
-fi
 
