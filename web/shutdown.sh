f831b8b52e6396d43afe91c15523cdb68da9bd48
Joseph C Wang <joequant@gmail.com>
2014-02-17 20:22:37 +0800

    add startup/shutdown

--- web/shutdown.sh
+++ web/shutdown.sh
@@ -4,7 +4,7 @@ sudo systemctl stop httpd
 #ipython notebook &
 
 if [ -d ../../OG-Platform ] ; then
-pushd ../../OG-Platform/examples/examples-bitquant/
+pushd ../../OG-Platform/examples/examples-simulated/
 mvn opengamma:server-stop -Dconfig=fullstack
 popd
 fi
--- web/startup.sh
+++ web/startup.sh
@@ -1,24 +1,26 @@
-#!/bin/bash -v
+#!/bin/bash
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $SCRIPT_DIR
 
 sudo systemctl restart httpd
 #ipython notebook &
 
 if [ -d ../../OG-Platform ] ; then
 pushd ../../OG-Platform/examples/examples-simulated/
-mvn install
-mvn opengamma:server-start -Dconfig=fullstack
+mvn install >> $SCRIPT_DIR/og.log
+mvn opengamma:server-start -Dconfig=fullstack >> $SCRIPT_DIR/web/og.log &
 popd
 fi
 
 if [ -d ../../ethercalc ] ; then
 pushd ../../ethercalc
-make &
+make >> $SCRIPT_DIR/ethercalc.log &
 popd
 fi
 
 if [ -d ../../etherpad-lite ] ; then
 pushd ../../etherpad-lite
-bin/run.sh &
+bin/run.sh >> $SCRIPT_DIR/etherpad.log &
 popd
 fi
 
