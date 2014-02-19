45535a4b94acac591ecd6ff8d3cfccfe15a587cc
Joseph C Wang <joequant@gmail.com>
2014-02-19 17:22:00 +0800

    add postgresql_devel requirement

--- rpm/install-web-deps.sh
+++ rpm/install-web-deps.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 #
 # Make is etherpad
+# postgresql-devel for etherpad-lite
 
-urpmi --no-suggests --auto python-flask apache apache apache-mod_wsgi ipython \
-make
+urpmi --no-suggests --auto python-flask apache apache apache-mod_wsgi \
+ipython make postgresql-devel
+
 
