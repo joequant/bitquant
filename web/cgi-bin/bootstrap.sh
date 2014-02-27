b016a601a1b45bcee1b13b9663e470177efd7f76
Joseph Wang <joequant@gmail.com>
2014-02-27 18:46:14 +0800

    make bootstrap setuid

--- web/cgi-bin/bootstrap.sh
+++ web/cgi-bin/bootstrap.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# set setuid so that it is run with the user which checked out the
+# orginal git
 
 echo "Content-type: text/plain"
 echo ""
@@ -6,3 +8,4 @@ set -e
 SCRIPT_DIR="$( cd "$( dirname "$(readlink -f "${BASH_SOURCE[0]}")")" && pwd )"
 echo "Running from directory $SCRIPT_DIR"
 . $SCRIPT_DIR/../../git/bootstrap.sh
+echo "(done)"
