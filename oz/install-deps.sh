dd537f89fffcd35cd2f7928b74b47a04435a4c7f
Joseph Wang <joequant@gmail.com>
2014-03-04 23:27:31 +0800

    remove pushd/popd output.  add lvm2 to rpm  list

--- oz/install-deps.sh
+++ oz/install-deps.sh
@@ -7,7 +7,7 @@
 urpmi --no-suggests --auto \
    oz qemu libguestfs-tools \
    libvirt-utils tigervnc \
-   dnsmasq dnsmasq-utils
+   dnsmasq dnsmasq-utils lvm2
 
 systemctl start libvirtd.service
 
--- oz/mkimg-bitstation-raw.sh
+++ oz/mkimg-bitstation-raw.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-pushd ~/.oz/images
+pushd ~/.oz/images > /dev/null
 rm -f Mageia.dsk Bitstation.dsk
-popd
+popd > /dev/null
 # There seems to be a problem with vdi image creates
 oz-install -a MageiaBitstation.auto mageia-net.tdl -c oz-rawcfg  -d3 -t 7200
 
-pushd ~/.oz/images
+pushd ~/.oz/images > /dev/null
 export TMPDIR=$HOME/tmp
 mkdir $TMPDIR
 virt-sparsify Mageia.dsk Bitstation.dsk
 rm -f Mageia.dsk
-popd
+popd > /dev/null
+
