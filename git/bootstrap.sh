e6e23bbbe364ea6a24691441b46d47e725e86ef9
Joseph C Wang <joequant@gmail.com>
2014-03-05 19:26:49 +0800

    redirect stderr to stdout

--- git/bootstrap.sh
+++ git/bootstrap.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 pushd $SCRIPT_DIR
-./setup-misc.sh
-./setup-og.sh
-./setup-quantlib.sh
-./rebuild-og.sh
-./rebuild-misc.sh
+./setup-misc.sh 2>&1 
+./setup-og.sh 2>&1
+./setup-quantlib.sh 2>&1
+./rebuild-og.sh 2>&1
+./rebuild-misc.sh 2>&1
 popd
