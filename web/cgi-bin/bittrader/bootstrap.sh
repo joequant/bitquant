4e4f6e52b17317b061ed3ed0e901f5764639188d
Joseph Wang <joequant@gmail.com>
2014-03-05 20:03:41 +0800

    add HOME variable

--- web/cgi-bin/bittrader/bootstrap.sh
+++ web/cgi-bin/bittrader/bootstrap.sh
@@ -2,11 +2,13 @@
 # set setuid so that it is run with the user which checked out the
 # orginal git
 
-echo "Content-type: text/plain"
+echo "Content-type: text/html"
 echo ""
-set -e
+echo "<pre>"
 SCRIPT_DIR=%SCRIPT_DIR%
 echo "Running from directory $SCRIPT_DIR as user "`whoami`
+export HOME=/home/`whoami`
 . $SCRIPT_DIR/../git/bootstrap.sh
 
 echo "(done)"
+echo "</pre>"
