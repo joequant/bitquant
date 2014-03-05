1f587d6e0e8a730f40ad59cbba6f6a9253da4e74
Joseph Wang <joequant@gmail.com>
2014-03-05 17:32:37 +0800

    add mkimg script

---
+++ oz/mkimg.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+CONFIG=$1
+FORMAT=$2
+# There seems to be a problem with vdi image created by oz
+# generate qcow2 image and then convert to vdi
+GENFORMAT=$FORMAT
+if [ $FORMAT = "vdi" ] ; then
+GENFORMAT=qcow2
+fi
+
+pushd ~/.oz/images > /dev/null
+rm -f Mageia.$GENFORMAT $CONFIG.$GENFORMAT
+popd > /dev/null
+# There seems to be a problem with vdi image creates
+oz-install -a Mageia$CONFIG.auto mageia-net.tdl -c oz-$GENFORMAT.cfg  -d3 -t 7200
+
+pushd ~/.oz/images > /dev/null
+export TMPDIR=$HOME/tmp
+mkdir $TMPDIR
+
+if [ $FORMAT = "vdi" ] ; then
+virt-sparsify Mageia.$GENFORMAT tmp.$$.$GENFORMAT
+qemu-img convert -O vdi tmp.$$.$GENFORMAT $CONFIG.vdi
+rm -f tmp.$$.$GENFORMAT
+else
+virt-sparsify Mageia.$GENFORMAT $CONFIG.$GENFORMAT
+fi
+rm -f Mageia.$GENFORMAT
+popd > /dev/null
+echo "Image $CONFIG.$FORMAT generated in directory ~/.oz/images"
+
