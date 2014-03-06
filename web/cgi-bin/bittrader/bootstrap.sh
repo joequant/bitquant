ab4d8b77664e0cc0c186ab4825eb9cf55ebca475
Joseph C Wang <joequant@gmail.com>
2014-03-07 07:38:30 +0800

    combine bootstrap into one file

--- web/cgi-bin/bittrader/bootstrap.sh
+++ web/cgi-bin/bittrader/bootstrap.sh
@@ -6,9 +6,11 @@ echo "Content-type: text/html"
 echo ""
 echo "<pre>"
 SCRIPT_DIR=%SCRIPT_DIR%
-echo "Running from directory $SCRIPT_DIR as user "`whoami`
 export HOME=/home/`whoami`
+echo "Running from directory $SCRIPT_DIR as user "`whoami`
+echo "Installing packages"
+. $SCRIPT_DIR/../rpm/install-build-deps.sh
+echo "Doing initial installation"
 . $SCRIPT_DIR/../git/bootstrap.sh
-
 echo "(done)"
 echo "</pre>"
--- web/cgi-bin/bittrader/install-packages.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Content-type: text/plain"
-echo ""
-set -e
-SCRIPT_DIR=%SCRIPT_DIR%
-echo "Running from directory $SCRIPT_DIR"
-. $SCRIPT_DIR/../rpm/install-build-deps.sh
-echo "Done"
