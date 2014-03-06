36e840ee9fca4efaaa3d807c57aeacf47d39d3fd
Joseph Wang <joequant@gmail.com>
2014-03-07 00:35:32 +0800

    clean up Makefile

--- oz/img-zero.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Execute as superuser
-sudo rmmod nbd
-sudo modprobe nbd max_part=16
-sudo qemu-nbd -c /dev/nbd0 $1
-sudo zerofree /dev/nbd0p1
-sudo qemu-nbd -d /dev/nbd0
-VBoxManage modifyhd $1  --compact
