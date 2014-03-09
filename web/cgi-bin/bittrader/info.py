296052ee8a6ef28b1fa21518801ab5e8f9cc1f00
Joseph C Wang <joequant@gmail.com>
2014-03-09 08:49:18 +0800

    bittrader info

--- web/cgi-bin/bittrader/info.py
+++ web/cgi-bin/bittrader/info.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python
 from wsgiref.handlers import CGIHandler
 from flask import Flask
+import subprocess
+import sys
+import shutil
+import os
 app = Flask(__name__)
 
 @app.route('/')
@@ -11,6 +15,37 @@ def index():
 def test():
     return 'test'
 
-if __name__ == '__main__':
-        from wsgiref.handlers import CGIHandler
-        CGIHandler().run(app)
+@app.route('/git-root')
+def git_root():
+    return os.path.expanduser(os.path.join("~", "git", "bitquant"))
+
+@app.route('/cgi-root')
+def cgi_root():
+    return "/var/www/cgi-bin/bittrader"
+
+@app.route("/refresh-scripts")
+def refresh_scripts():
+    retval = "Refreshing scripts\n"
+    local_cgi_path = os.path.join(git_root(),
+                                  "web", "cgi-bin", "bittrader")
+    for file in os.listdir(cgi_root()):
+        retval = retval + "removing old " + file + "\n"
+        os.remove(os.path.join(cgi_root(), file))
+    for file in os.listdir(local_cgi_path):
+        if file.endswith(".sh") or file.endswith(".py"):
+            shutil.copy2(os.path.join(local_cgi_path, file), cgi_root())
+            retval = retval + "copying " + file + "\n"
+    return retval + "(done)"
+
+
+@app.route("/version")
+def version():
+    os.chdir(git_root())
+    return subprocess.check_output(["git", "rev-parse", "--short", "HEAD"])
+
+
+if __name__ == '__main__' and len(sys.argv) == 1:
+    from wsgiref.handlers import CGIHandler
+    CGIHandler().run(app)
+elif __name__ == '__main__' and sys.argv[1] == "refresh-scripts":
+    print refresh_scripts() + "\n"
--- web/cgi-bin/bittrader/refresh.sh
+++ web/cgi-bin/bittrader/refresh.sh
@@ -1,13 +1,5 @@
 #!/bin/bash 
 echo "Content-type: text/plain"
 echo ""
-echo "Refresh scripts"
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-ME=`stat -c "%U" ${BASH_SOURCE[0]}`
-GROUP=`stat -c "%G" ${BASH_SOURCE[0]}`
-export HOME=/home/`whoami`
-GIT_DIR=$HOME/git/bitquant
-
-cp $GIT_DIR/web/cgi-bin/bittrader/*.sh /var/www/cgi-bin/bittrader
-cp $GIT_DIR/web/cgi-bin/bittrader/*.py /var/www/cgi-bin/bittrader
-echo "Done"
+python $SCRIPT_DIR/info.py refresh-scripts
