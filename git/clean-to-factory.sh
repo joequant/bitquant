e4acec10d860e9f90d2f4acf67a9e37c1c3d087b
Joseph C Wang <joequant@gmail.com>
2014-03-10 13:00:10 +0800

    add clean to factory script

---
+++ git/clean-to-factory.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+MY_NAME=joequant
+cd $SCRIPT_DIR
+
+pushd ../..
+rm -rf ethercalc etherpad-lite OG-Platform OG-PlatformNative \
+   OG-Tools JSurface3D quantlib
+popd
