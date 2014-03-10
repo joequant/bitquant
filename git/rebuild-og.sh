5ba7770b49d0a7c123ea51f664216feee9b56f50
Joseph C Wang <joequant@gmail.com>
2014-03-10 20:08:03 +0800

    add norootchecks to rebuilds

--- git/rebuild-og.sh
+++ git/rebuild-og.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $SCRIPT_DIR
+. ../web/norootcheck.sh
 pushd ../../OG-Platform  > /dev/null
 pushd examples/examples-simulated  > /dev/null
 mvn opengamma:server-stop -Dconfig=fullstack 
