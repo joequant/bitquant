9191836a3a5ab7b9da1b0a0dd94477b6a27daaca
Joseph C Wang <joequant@gmail.com>
2014-03-12 15:48:42 +0800

    startup

--- web/startup.sh
+++ web/startup.sh
@@ -1,30 +1,29 @@
 #!/bin/bash
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+LOG_DIR=$SCRIPT_DIR/log
 cd $SCRIPT_DIR
 . norootcheck.sh
 
-echo "Restarting apache"
-sudo systemctl restart httpd
 #ipython notebook &
 
 if [ -d ../../OG-Platform ] ; then
 echo "Restarting opengamma"
 pushd ../../OG-Platform/examples/examples-simulated/ > /dev/null
-(mvn install >> $SCRIPT_DIR/og.log; mvn opengamma:server-start -Dconfig=fullstack >> $SCRIPT_DIR/og.log) &
+(mvn install >> $LOG_DIR/og.log; mvn opengamma:server-start -Dconfig=fullstack >> $LOG_DIR/og.log) &
 popd > /dev/null
 fi
 
 if [ -d ../../ethercalc ] ; then
 pushd ../../ethercalc > /dev/null
 echo "Restarting ethercalc"
-make >> $SCRIPT_DIR/ethercalc.log 2>&1 &
+make >> $LOG_DIR/ethercalc.log 2>&1 &
 popd > /dev/null
 fi
 
 if [ -d ../../etherpad-lite ] ; then
 echo "Restarting etherpad"
 pushd ../../etherpad-lite > /dev/null
-bin/run.sh >> $SCRIPT_DIR/etherpad.log 2>&1 &
+bin/run.sh >> $LOG_DIR/etherpad.log 2>&1 &
 popd > /dev/null
 fi
 
