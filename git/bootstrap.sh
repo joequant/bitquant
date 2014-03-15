af9d9502947ac2503c633ec24fe77cf4dfc27248
Joseph C Wang <joequant@gmail.com>
2014-03-15 17:44:12 +0800

    clean up pushd/popd

--- git/bootstrap.sh
+++ git/bootstrap.sh
@@ -2,11 +2,11 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 set -e
 
-pushd $SCRIPT_DIR
+pushd $SCRIPT_DIR > /dev/null
 ./setup-misc.sh 2>&1 
 ./setup-og.sh 2>&1
 ./setup-quantlib.sh 2>&1
 ./rebuild-og.sh 2>&1
 ./rebuild-misc.sh 2>&1
 touch ../web/bootstrap.done
-popd
+popd > /dev/null
