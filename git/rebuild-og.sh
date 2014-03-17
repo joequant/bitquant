aedfc8d6e7422a93bb9b6c4fa43958434f3c4527
Joseph C Wang <joequant@gmail.com>
2014-03-17 20:51:25 +0800

    rebuild only example-simulated

--- git/rebuild-og.sh
+++ git/rebuild-og.sh
@@ -3,8 +3,8 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $SCRIPT_DIR
 . ../web/norootcheck.sh
 pushd ../../OG-Platform  > /dev/null
-mvn install -Dmaven.test.skip=True
 pushd examples/examples-simulated > /dev/null
+mvn install -Dmaven.test.skip=True
 mvn opengamma:server-stop -Dconfig=fullstack
 mvn opengamma:server-init -Dconfig=fullstack
 mvn opengamma:server-start -Dconfig=fullstack
