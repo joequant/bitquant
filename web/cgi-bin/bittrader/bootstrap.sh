bda7c29bce9ebb6de1c9a43a6d893f3181736d2f
Joseph C Wang <joequant@gmail.com>
2014-03-22 03:03:02 +0800

    add bootstrap locking

--- web/cgi-bin/bittrader/bootstrap.sh
+++ web/cgi-bin/bittrader/bootstrap.sh
@@ -15,19 +15,21 @@ LOG_DIR=$GIT_DIR/web/log
 flock -x -n 200 || exit 1
 # Redirect STDERR to STDOUT
 echo "Saving to <a href='log/bootstrap.log' target='_blank'>log file</a>"
+rm -f $LOG_DIR/bootstrap.log
 exec 2>&1
 exec 6>&1
 exec > $LOG_DIR/bootstrap.log
 
 echo "Running from directory $GIT_DIR as user "`whoami`
 echo "Installing packages"
-. $GIT_DIR/web/install-build-deps.sh
+$GIT_DIR/web/install-build-deps.sh
 echo "Doing initial installation"
-. $GIT_DIR/git/bootstrap.sh
+$GIT_DIR/git/bootstrap.sh
 echo "Starting up servers"
 sudo systemctl enable bitquant
 sudo systemctl start bitquant
 echo "(done)"
 echo 1>&6 6>&~
-) 200> $LOG_DIR/bootstrap.lock || echo "Already running see progress in <a href='log/bootstrap.log' target='_blank'>log file</a>" 
+) 200> $LOG_DIR/bootstrap.lock &
+echo "See progress in <a href='log/bootstrap.log' target='_blank'>log file</a>" 
 echo "</pre>"
