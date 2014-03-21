352eabb8687ad9a02b1e6173e683b118a0a9a8c1
Joseph C Wang <joequant@gmail.com>
2014-03-22 03:09:08 +0800

    more bootstrap modifications

--- web/cgi-bin/bittrader/bootstrap.sh
+++ web/cgi-bin/bittrader/bootstrap.sh
@@ -16,14 +16,13 @@ flock -x -n 200 || exit 1
 # Redirect STDERR to STDOUT
 echo "Saving to <a href='log/bootstrap.log' target='_blank'>log file</a>"
 rm -f $LOG_DIR/bootstrap.log
-exec 2>&1
 exec 6>&1
 exec > $LOG_DIR/bootstrap.log
 
 echo "Running from directory $GIT_DIR as user "`whoami`
 echo "Installing packages"
-$GIT_DIR/web/install-build-deps.sh
-echo "Doing initial installation"
+$GIT_DIR/web/install-build-deps.sh 2>&1
+echo "Doing initial installation" 2>&1
 $GIT_DIR/git/bootstrap.sh
 echo "Starting up servers"
 sudo systemctl enable bitquant
