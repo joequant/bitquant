0a5b2c41a1ec6f7ed8225a861e8a2e7dee72e9e9
Joseph Wang <joequant@gmail.com>
2014-03-26 00:58:58 +0800

    initial tag commit

---
+++ git/mktag.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $SCRIPT_DIR
+. repos-info.sh
+
+if [ "$#" != "1" ] ; then
+echo "incorrect number of args $#"
+exit 1
+fi
+
+tag=$1
+echo "Tag $1"
+
+pushd ../.. > /dev/null
+for repo in $repos_misc $repos_quantlib $repos_og ; do
+echo git tag $repo $tag
+done
+popd > /dev/null
