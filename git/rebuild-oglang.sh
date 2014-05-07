3e0b68ac1a0aef71d7d09049d5f15fab8d094355
Joseph C Wang <joequant@gmail.com>
2014-05-07 20:44:06 +0800

    add changes to oglang

--- git/rebuild-oglang.sh
+++ git/rebuild-oglang.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 sudo mkdir -p /var/run/opengamma
 sudo chmod a+rwx /var/run/opengamma
@@ -7,17 +8,12 @@ sudo mkdir -p /var/log/OG-RStats
 sudo chmod a+rwx /var/log/OG-RStats
 
 mkdir -p ~/etc/OpenGammaLtd
-echo <<EOF > ~/etc/OpenGammaLtd/LanguageIntegration
-jvmLibrary=/usr/lib/jvm/java-1.7.0/jre/lib/amd64/server/libjvm.so
-jvmProperty.opengamma.configuraton.url=http://localhost:8080/jax/configuration/0/
-EOF
 
-echo <<EOF > ~/etc/OpenGammaLtd/OpenGammaR
-serviceExecutable=/home/joe/git/OG-PlatformNative/og-language/target/run/Debug/ServiceRunner
-connectorLogConfiguration=/home/joe/git/OG-PlatformNative/og-language/src/package/ai/log4cxx.properties
-EOF
+echo "jvmLibrary=/usr/lib/jvm/java-1.7.0/jre/lib/amd64/server/libjvm.so
+jvmProperty.opengamma.configuraton.url=http://localhost:8080/jax/configuration/0/" > ~/etc/OpenGammaLtd/LanguageIntegration
 
-
+echo "serviceExecutable=/home/joe/git/OG-PlatformNative/og-language/target/run/Debug/ServiceRunner
+connectorLogConfiguration=/home/joe/git/OG-PlatformNative/og-language/src/package/ai/log4cxx.properties" > ~/etc/OpenGammaLtd/OpenGammaR
 
 pushd  ../../OG-PlatformNative
 export MVN_ARGS="-X -Dmaven.test.skip=true"
@@ -27,10 +23,13 @@ cat <<EOF > exe-kill
 echo $*
 EOF
 chmod a+x exe-kill
-ant clean
 ant configure -Dprofile.nix=true -Dtool.cpptasks=true 
 # -Dtool.r=true
-ant install -v
+ant install -v -Dskip.tests=true
+pushd /home/joe/git/OG-PlatformNative/og-language/target/run/Debug
+./ServiceRunner run >& $SCRIPT_DIR/../web/log/ServiceRunner.log &
+popd
+sleep 10
 pushd og-rstats/target/package
 export OG_RSTATS_TARGET=../../../
 R CMD INSTALL OpenGamma
