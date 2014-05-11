6b19ed635687c8dd53aed8fb44712424cfe31db8
Joseph C Wang <joequant@gmail.com>
2014-05-12 07:58:37 +0800

    add vbox script

---
+++ oz/mkvbox.sh
@@ -0,0 +1,11 @@
+VBoxManage modifyvm  Bitstation --hda none
+VBoxManage unregistervm Bitstation --delete
+VBoxManage createvm --name "Bitstation" --ostype Other_64 --register
+VBoxManage modifyvm  "Bitstation" --memory 2048 --nic1 bridged \
+   --nictype1 virtio \
+   --pae on
+VBoxManage storagectl Bitstation --name "SATA Controller" --add sata
+VBoxManage storageattach Bitstation --storagectl "SATA Controller" \
+   --port 0 --device 0 --type hdd --medium ~/.oz/images/bit1.vdi
+
+
