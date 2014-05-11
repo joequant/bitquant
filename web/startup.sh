22aee42a6a6a0dea235e485415b462700e9d5195
Joseph C Wang <joequant@gmail.com>
2014-05-12 06:49:11 +0800

    disable output timeout

--- web/startup.sh
+++ web/startup.sh
@@ -23,7 +23,7 @@ sudo chmod a+rwx /var/log/OG-RStats
 
 mkdir -p ~/etc/OpenGammaLtd
 echo "jvmLibrary=/usr/lib/jvm/java-1.7.0/jre/lib/amd64/server/libjvm.so
-idleTimeout=30000000
+idleTimeout=0xFFFFFFFF
 jvmProperty.opengamma.configuraton.url=http://localhost:8080/jax/configuration/0/" > ~/etc/OpenGammaLtd/LanguageIntegration
 
 #serviceExecutable=/home/joe/git/OG-PlatformNative/og-language/target/run/Release/ServiceRunner
