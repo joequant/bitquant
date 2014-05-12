0e48310fe5adf8e80ecb13141d0cb8e94016f6fc
Joseph C Wang <joequant@gmail.com>
2014-05-12 17:31:21 +0800

    add mkvbox.sh changes

--- oz/mkvbox.sh
+++ oz/mkvbox.sh
@@ -3,7 +3,13 @@ VBoxManage unregistervm Bitstation --delete
 VBoxManage createvm --name "Bitstation" --ostype Other_64 --register
 VBoxManage modifyvm  "Bitstation" --memory 2048 --nic1 bridged \
    --nictype1 virtio \
-   --pae on
+   --pae on \
+   --cpus 2 \
+   --accelerate3d on \
+   --vrde on \
+   --audio pulse \
+   --rtcuseutc on \
+   --mouse usbmultitouch
 VBoxManage storagectl Bitstation --name "SATA Controller" --add sata
 VBoxManage storageattach Bitstation --storagectl "SATA Controller" \
    --port 0 --device 0 --type hdd --medium ~/.oz/images/bit1.vdi
