4e8950c9e2b1d9d841a5a0f5e1220974949ee829
Joseph C Wang <joequant@gmail.com>
2014-05-14 23:21:12 +0800

    separate out initialization

--- git/init-og.sh
+++ git/init-og.sh
@@ -14,16 +14,8 @@ cd $SCRIPT_DIR
 . ../web/norootcheck.sh
 pushd ../../OG-Platform  > /dev/null
 pushd examples/examples-simulated > /dev/null
-mvn -T$ncpus install -Dmaven.test.skip=True
+mvn opengamma:server-stop -Dconfig=fullstack
+mvn opengamma:server-init -Dconfig=fullstack
 popd > /dev/null
 popd > /dev/null
-
-
-if [ -n "$OG_COMPILE_PLATFORM_NATIVE" ]; then
-pushd ../../OG-Tools/corporate-parent > /dev/null
-mvn -T$ncpus install
-popd > /dev/null
-
-source $SCRIPT_DIR/rebuild-oglang.sh
-fi
 
