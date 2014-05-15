3673becd6eab1ae1acc9f422b146d6391efe7584
Joseph C Wang <joequant@gmail.com>
2014-05-16 00:01:28 +0800

    mkvbox.sh networking tweaks

--- oz/mkvbox.sh
+++ oz/mkvbox.sh
@@ -1,10 +1,22 @@
 VBoxManage modifyvm Bitstation --hda none
-VBoxManage closemedium disk ~/.oz/images/bit1.vdi
 VBoxManage unregistervm Bitstation
 rm -rf ~/VirtualBox\ VMs/Bitstation/
+VBoxManage dhcpserver remove --ifname vboxnet0
+VBoxManage hostonlyif remove vboxnet0
+VBoxManage hostonlyif create
+VBoxManage hostonlyif ipconfig vboxnet0 --dhcp
+VBoxManage dhcpserver add --ifname vboxnet0 \
+   --ip 192.168.56.100 \
+   --netmask 255.255.255.0 \
+   --lowerip 192.168.56.101 \
+   --upperip 192.168.56.255 --enable
+
 VBoxManage createvm --name "Bitstation" --ostype Other_64 --register
-VBoxManage modifyvm  "Bitstation" --memory 2048 --nic1 bridged \
+VBoxManage modifyvm  "Bitstation" --memory 2048 \
+   --nic1 nat \
+   --nic2 hostonly \
    --nictype1 virtio \
+   --nictype2 virtio \
    --pae on \
    --cpus 2 \
    --accelerate3d on \
@@ -12,7 +24,7 @@ VBoxManage modifyvm  "Bitstation" --memory 2048 --nic1 bridged \
    --audio pulse \
    --rtcuseutc on \
    --mouse usbmultitouch \
-   --bridgeadapter1 eth0
+   --hostonlyadapter2 vboxnet0
 VBoxManage storagectl Bitstation --name "SATA Controller" --add sata
 VBoxManage storageattach Bitstation --storagectl "SATA Controller" \
    --port 0 --device 0 --type hdd --medium ~/.oz/images/bit1.vdi
