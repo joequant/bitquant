0f66bb11d26d0269e3ab989fa0d61bd6a1837201
Joseph C Wang <joequant@gmail.com>
2014-06-09 11:31:08 +0800

    add init data

---
+++ web/cgi-bin/bittrader/init-data.sh
@@ -0,0 +1,41 @@
+#!/bin/bash 
+# set setuid so that it is run with the user which checked out the
+# orginal git
+
+# would like to set -e, but this causes odd errors
+# set -e
+
+echo "Content-type: text/plain"
+echo ""
+
+TAG=init-data
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+export HOME=/home/`whoami`
+GIT_DIR=$HOME/git/bitquant
+LOG_DIR=$GIT_DIR/web/log
+
+if [ -e $LOG_DIR/$TAG.done ] ; then
+rm -f $LOG_DIR/$TAG.done
+rm -f $LOG_DIR/$TAG.log
+fi
+
+(
+flock -x -n 200 || (echo "process already running" ; exit 1)
+# Redirect STDERR to STDOUT
+exec 6>&1
+exec > $LOG_DIR/$TAG.log
+exec 2>&1
+
+echo "Running from directory $GIT_DIR as user "`whoami`
+echo "Shutting down servers"
+sudo systemctl stop bitquant
+echo "Doing init data"
+$GIT_DIR/git/init-og.sh
+echo "Starting up servers"
+sudo systemctl enable bitquant
+sudo systemctl start bitquant
+touch $LOG_DIR/$TAG.done
+echo "(done)"
+exec 1>&6
+) 200> $LOG_DIR/$TAG.lock &
+
