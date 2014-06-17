6fcef0717d27bbaa64cb8aa81422b41021a6f166
Joseph C Wang <joequant@gmail.com>
2014-06-17 16:06:30 +0800

    fix typo. add install r deps

--- web/cgi-bin/bittrader/bootstrap.sh
+++ web/cgi-bin/bittrader/bootstrap.sh
@@ -31,9 +31,11 @@ echo "Running from directory $GIT_DIR as user "`whoami`
 echo "Shutting down servers"
 sudo systemctl stop bitquant
 echo "Installing packages"
-source $GIT_DIR/web/script/install-build-deps.sh
+source $GIT_DIR/web/scripts/install-build-deps.sh
 echo "Doing initial installation"
 $GIT_DIR/git/bootstrap.sh
+echo "Installing R packages"
+$GIT_DIR/web/scripts/install-r-pkgs.sh
 echo "Starting up servers"
 sudo systemctl enable bitquant
 sudo systemctl start bitquant
