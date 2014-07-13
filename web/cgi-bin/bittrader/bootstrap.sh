5aa5036534175b75408745fa1e579e51ce148afb
Joseph C Wang <joequant@gmail.com>
2014-07-13 08:13:27 +0800

    make ipython examples a symlink

--- web/cgi-bin/bittrader/bootstrap.sh
+++ web/cgi-bin/bittrader/bootstrap.sh
@@ -43,7 +43,10 @@ echo "Set up wiki"
 ./conf.sh /wiki-init
 echo "Set up ipython"
 mkdir -p $MY_HOME/ipython
-cp -r $GIT_DIR/web/home/ipython/* $MY_HOME/ipython
+if [ -d $MY_HOME/ipython/examples ] ; then
+mv $MY_HOME/ipython/examples $MY_HOME/ipython/examples.bak
+fi
+ln -s -f ../git/bitquant/web/home/ipython/examples $MY_HOME/ipython/examples
 echo "Set up R"
 mkdir -p $MY_HOME/R
 cp -r $GIT_DIR/web/home/R/* $MY_HOME/R
