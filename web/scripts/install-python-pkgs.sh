2aea9a5efc875779b14c46e5236c9aed689725ee
Joseph C Wang <joequant@gmail.com>
2014-07-20 00:54:05 +0800

    install python pkg

--- web/scripts/install-python-pkgs.sh
+++ web/scripts/install-python-pkgs.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+pushd $SCRIPT_DIR > /dev/null
 . norootcheck.sh
+popd > /dev/null
 sudo pip install --upgrade Quandl rpy2
