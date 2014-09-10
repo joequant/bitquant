34d0f2ba24381d661532cc16df2271bb83e529fe
Joseph C Wang <joequant@gmail.com>
2014-09-11 02:37:34 +0800

    lxc fixes

--- oz/lxc/mklxc.sh
+++ oz/lxc/mklxc.sh
@@ -1,5 +1,6 @@
 if [ ! -d vdi1 ] ; then
-sudo cp -r vdi vdi1
+sudo cp -r -a vdi vdi1
+sudo rm -f vdi1/etc/systemd/system/getty.target.wants/console-shell.service
 fi
 
 sudo lxc-create -f myvm.conf -n bitstation
