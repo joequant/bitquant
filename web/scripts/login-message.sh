14e9ce66336fb18e86120a8d0ceaf7b3777aa065
Joseph C Wang <joequant@gmail.com>
2014-09-11 17:02:38 +0800

    modify login message

--- web/scripts/login-message.sh
+++ web/scripts/login-message.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-IPV4_ADDR=$(/sbin/ifconfig | grep "inet addr" | grep -v 10.0 | grep -v 127.0 | awk '{print $2;}' | sed -e 's/addr://' | head -1)
+IPV4_ADDR=$(/sbin/ifconfig | grep "inet" | grep -v "inet6" | grep -v 10.0 | grep -v 127.0 | awk '{print $2;}' | sed -e 's/addr://' | head -1)
 HOSTNAME=$(hostname)
 
 echo "Connect with http://$IPV4_ADDR/"
