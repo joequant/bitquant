edede6a2972ed77dbbba34eccfffc0bbf568853e
Joseph C Wang <joequant@gmail.com>
2014-10-22 17:33:06 +0800

    softcode redirect

--- oz/lxc/redirect.sh
+++ oz/lxc/redirect.sh
@@ -1,6 +1,8 @@
+#!/bin/bash
+dest=$1
 iptables -t nat -F PREROUTING
 iptables -t nat -F POSTROUTING
 iptables -F FORWARD
-iptables -A PREROUTING -t nat -i enp3s0 -p tcp --dport 80 -j DNAT --to 192.168.122.225:80
-iptables -A FORWARD -p tcp -d 192.168.1.225 --dport 80 -j ACCEPT
+iptables -A PREROUTING -t nat -i enp3s0 -p tcp --dport 80 -j DNAT --to $dest:80
+iptables -A FORWARD -p tcp -d $dest --dport 80 -j ACCEPT
 iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE 
