e2f3df53de4fa627b038b4e741f68c465c97a524
Joseph C Wang <joequant@gmail.com>
2014-10-29 20:40:00 +0800

    remove build deps.sh

---
+++ web/scripts/remove-build-deps.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# These are all of the packages that need to be installed before bootstrap
+# is run
+# 
+# Note that all apache modules should have already been installed
+# in the bootstrap image.  Otherwise you will have the system attempt
+# to reload httpd which causes the httpd connection to go down
+#
+# dokuwiki also needs to be in bootstrap for the same reasons
+
+if grep -q Cauldron /etc/release  ; then 
+JAVA=java-1.8.0-openjdk-devel
+fi
+
+if [[ $UID -ne 0 ]]; then
+  SUDO=sudo
+fi
+
+
+$SUDO urpme \
+--auto \
+make \
+postgresql9.3-devel \
+cmake \
+gcc-c++
+
+
+$SUDO urpme --auto-orphans --auto
+
+
+
+
+
+
+
+
+
+
