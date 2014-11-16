b1259da0a21c65a300747886fb97b693c973b8fc
Joseph C Wang <joequant@gmail.com>
2014-11-17 03:27:28 +0800

    remove sudo from setup-mifos.sh

--- git/setup-mifos.sh
+++ git/setup-mifos.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
+if ! $(groups $(whoami) | grep &>/dev/null '\btomcat\b'); then
+    echo "User not in tomcat group"
+    exit
+fi
+
 export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.40-4.b02.4.mga5.x86_64/jre
 export TOMCAT_HOME=/var/lib/tomcat
 mysqladmin -uroot -pmysql create 'mifosplatform-tenants'
 mysqladmin -uroot -pmysql create 'mifostenant-default'
+mkdir -p ~/tmp
+pushd ~/tmp
 if [ ! -d mifosplatform-1.25.1.RELEASE ] ; then
 curl --location http://downloads.sourceforge.net/project/mifos/Mifos%20X/mifosplatform-1.25.1.RELEASE.zip -O mifosplatform-1.25.1.RELEASE.zip
 unzip mifosplatform-1.25.1.RELEASE.zip
@@ -17,12 +24,10 @@ echo "" | sudo keytool -genkey -alias mifostom -keyalg RSA \
   -dname "CN=Unknown, OU=Unknown, O=Unknown, L=Unknown,S=Unknown, C=Unknown" \
   -keystore /usr/share/tomcat/.keystore
 fi
-sudo cp mifosng-provider.war $TOMCAT_HOME/webapps
-sudo mkdir $TOMCAT_HOME/webapps/ROOT
-sudo cp -r api-docs $TOMCAT_HOME/webapps/ROOT
-sudo cp -r apps/community-app $TOMCAT_HOME/webapps/ROOT
+cp mifosng-provider.war $TOMCAT_HOME/webapps
+mkdir $TOMCAT_HOME/webapps/ROOT
+cp -r api-docs $TOMCAT_HOME/webapps/ROOT
+cp -r apps/community-app $TOMCAT_HOME/webapps/ROOT
 popd
-sudo cp server.xml /etc/tomcat
-
-
-
+cp server.xml /etc/tomcat
+popd
