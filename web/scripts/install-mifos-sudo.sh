47b90254bd9853d73437fd44e8c48766f02b7934
Joseph C Wang <joequant@gmail.com>
2014-11-21 05:17:44 +0800

    remove skip networking before starting mysqld

--- web/scripts/install-mifos-sudo.sh
+++ web/scripts/install-mifos-sudo.sh
@@ -5,10 +5,12 @@ ME=$2
 
 . $ROOT_SCRIPT_DIR/rootcheck.sh
 # Mifos set up
+sed -i -e s/^skip-networking/#skip-networking/ /etc/my.cnf
+
 systemctl enable mysqld
 systemctl restart mysqld
 if [ -e /bin/mysqladmin ] ; then
 mysqladmin password mysql
 fi
 usermod -a -G tomcat $ME
-sed -i -e s/^skip-networking/#skip-networking/ /etc/my.cnf
+
