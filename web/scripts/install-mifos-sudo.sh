47c0c38be02eb52da947936b0c583cfea7e7926b
Joseph C Wang <joequant@gmail.com>
2014-11-22 13:11:19 +0800

    move directory setup to sudo

--- web/scripts/install-mifos-sudo.sh
+++ web/scripts/install-mifos-sudo.sh
@@ -21,3 +21,21 @@ echo "" | keytool -genkey -alias mifostom -keyalg RSA \
   -keystore /usr/share/tomcat/.keystore
 fi
 
+export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.40-4.b02.4.mga5.x86_64/jre
+export TOMCAT_HOME=/var/lib/tomcat
+mkdir -p ~/tmp
+pushd ~/tmp
+if [ ! -d mifosplatform-1.25.1.RELEASE ] ; then
+curl --location http://downloads.sourceforge.net/project/mifos/Mifos%20X/mifosplatform-1.25.1.RELEASE.zip -O mifosplatform-1.25.1.RELEASE.zip
+unzip mifosplatform-1.25.1.RELEASE.zip
+fi
+pushd mifosplatform-1.25.1.RELEASE
+mysql -uroot -pmysql mifosplatform-tenants < database/mifospltaform-tenants-first-time-install.sql
+
+cp mifosng-provider.war $TOMCAT_HOME/webapps
+mkdir -p $TOMCAT_HOME/webapps/ROOT
+cp -r api-docs $TOMCAT_HOME/webapps/ROOT
+cp -r apps/community-app $TOMCAT_HOME/webapps/ROOT
+popd
+cp $SCRIPT_DIR/server.xml /etc/tomcat
+popd
