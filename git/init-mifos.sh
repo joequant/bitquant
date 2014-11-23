0b64836999859990e060553a75bef6dad53895f0
Joseph C Wang <joequant@gmail.com>
2014-11-23 17:36:40 +0800

    remove tmp files

--- git/init-mifos.sh
+++ git/init-mifos.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-mkdir -p ~/tmp
-pushd ~/tmp
+mkdir -p ~/tmp-mifos
+pushd ~/tmp-mifos
 if [ ! -d mifosplatform-1.25.1.RELEASE ] ; then
 curl --location http://downloads.sourceforge.net/project/mifos/Mifos%20X/mifosplatform-1.25.1.RELEASE.zip -O mifosplatform-1.25.1.RELEASE.zip
 unzip mifosplatform-1.25.1.RELEASE.zip
@@ -8,4 +8,5 @@ fi
 pushd mifosplatform-1.25.1.RELEASE
 mysql -uroot -pmysql mifostenant-default <  database/migrations/sample_data/load_sample_data.sql
 popd
+rm -rf ~/tmp-mifos
 
