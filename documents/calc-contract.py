5a422c159f02e3a247cbdc70fb19166b6b598cbd
Joseph C Wang <joequant@gmail.com>
2014-11-26 08:05:36 +0800

    add contract

---
+++ documents/calc-contract.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python
+from datetime import date
+from money import Money
+from dateutil.relativedelta import relativedelta
+from decimal import Decimal
+import findates
+from loan import LoanContract
+
+class LoanCalculator(object):
+    def __init__(self):
+        self.principal = None
+        self.interest = None
+    def calculate(self, contract):
+        self.currency = contract.currency
+        self.principal = Money(0.0, self.currency)
+        self.interest = Money(0.0, self.currency)
+        self.prev_event_date = None
+        self.contract = contract
+        contract.payments(self)
+    def show_payments(self, contract):
+        self.calculate(contract)
+    def fund(self, on, amount,
+             payment_type=None):
+        if callable(amount):
+            payment = amount()
+        else:
+            payment = amount
+        if self.prev_event_date != None:
+            self.interest = self.interest + \
+                 self.contract.interest(self.prev_event_date,
+                                   on,
+                                   (self.principal + self.interest))
+        self.prev_event_date = on
+        self.principal = self.principal + payment
+        print "Funding"
+        print on, payment, self.principal, self.interest
+    def payment(self, on,
+                amount,
+                payment_type=["principal"],
+                settlement_ccy=None,
+                optional=False):
+        if callable(amount):
+            payment = amount()
+        else:
+            payment = amount
+        if self.prev_event_date != None:
+            self.interest = self.interest + \
+                 self.contract.interest(self.prev_event_date,
+                                   on,
+                                   (self.principal + self.interest))
+        self.prev_event_date = on
+        if "principal" in payment_type:
+            self.principal = self.principal - payment
+        if "interest" in payment_type:
+            self.interest = self.interest - interest
+        print "Payment"
+        print on, payment, self.principal, self.interest
+
+    def remaining_principal(self, d2):
+        return lambda : self.principal
+    def accrued_interest(self, d1):
+        return lambda : self.interest
+
+if __name__ == "__main__":
+    my_contract = LoanContract()
+    my_contract.set_events({"kickstarter_success":True,
+                            "kickstarter_start_date": date(2015,1,1),
+                            "kickstarter_payment_date":date(2015,1,15),
+                            "kickstarter_revenue":50000,
+                            "early_payments":[]})
+    calculator = LoanCalculator()
+    calculator.show_payments(my_contract)
+    
