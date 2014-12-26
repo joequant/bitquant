f008e507a9db8063809654510a140b32be504840
joequant <joequant@gmail.com>
2014-12-27 01:02:55 +0800

    test change
    test

--- smart-contracts/LoanCalculator.js
+++ smart-contracts/LoanCalculator.js
@@ -1,5 +1,6 @@
 // Copyright (c) 2014, Bitquant Research Laboratories (Asia) Ltd.
 // Licensed under the Simplified BSD License
+
 "use strict";
 if (typeof define !== 'function') {
     var define = require('amdefine')(module);
