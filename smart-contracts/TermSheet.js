f31f1f6a82b75b1dbeba8b69540c7cf3ee2e07a8
henryyu731 <henryyu731@gmail.com>
2014-12-28 23:32:08 +0800

    Update TermSheet.js
    Please note that: 
    
    (i) Have not reviewed the part underneath Clause 16.
    (ii) It is currently unclear which part is Schedule A, Schedule B and Schedule C.
    (iii) Currently, there is no mention about the PREPAYMENT. 
    (iv) all the CONDITIONS PRECEDENT have missed out - think you have removed them on purpose. In such case, you would have to communicate well with the Borrower in advance as to what sort of Conditions Precedent need to be satisfied PRIOR TO the signing of this Smart Contract.

--- smart-contracts/TermSheet.js
+++ smart-contracts/TermSheet.js
@@ -14,7 +14,9 @@ Javascript code: Copyright (c) 2014 Bitquant Research Laboratories (Asia) Ltd.
 
 Released under terms of the Simplified BSD License.
 
-This FACILITY AGREEMENT is dated ****  between
+This FACILITY AGREEMENT is dated ****  
+
+BETWEEN
 
 1) YOYODYNE PROPULSION SYSTEMS Ltd incorporated and registered in Hong
 Kong with company number 1234552 whose registered office is at 1600
@@ -24,27 +26,15 @@ Pennsylvia Avenue (BORROWER)
 company number 2334455 whose registered office is at 221B Baker Street.
 (Lender).
 
-1.Governing law and jurisdiction
-------------------------------
-  1. This Agreement and any dispute or claim arising out of or in
-  connection with it or its subject matter or formation (including
-  non-contractual disputes or claims) shall be governed by and construed
-  in accordance with the law of Hong Kong.  
-  2. Each party irrevocably agrees that, subject as provided below, the
-  courts of Hong Kong shall have non-exclusive jurisdiction over any
-  dispute or claim that arises out of, or in connection with this
-  Agreement or its subject matter or formation (including
-  non-contractual disputes or claims). Nothing in this clause shall
-  limit the right of the Lender to take proceedings against the Borrower
-  in any other court of competent jurisdiction, nor shall the taking of
-  proceedings in any one or more jurisdictions preclude the taking of
-  proceedings in any other jurisdictions, whether concurrently or not,
-  to the extent permitted by the law of such other jurisdiction.
+BACKGROUND
 
-Definitions
+The Lender has agreed to provide the Borrower with an unsecured term loan facility of HK$[ ]
+
+AGREED TERMS
+
+1. Definitions
 -----------
-
-  1 Definitions - The following definitions apply in this Agreement:  
+1.1 The following definitions apply in this Agreement:  
   * Availability Period: the period from and including the date of this Agreement to and including the Final Repayment Date.  
   * BTC Exchange Rate:  the 24-hour average price in USD/BTC as reported for by Quandl (https://www.quandl.com/c/markets/bitcoin-data) for the second business day before the Final Repayment Date converted to HKD with the closing price as reported by Bloomberg (http://www.bloomberg.com/quote/USDHKD:CUR) for the second business day before the Final Repayment Date. 
   * Business Day: a day other than a Saturday, Sunday or public holiday when banks in Hong Kong are open for business.  
@@ -55,53 +45,72 @@ Definitions
   * Hong Kong: the Hong Kong Special Administration Region of the People’s Republic of China.  
   * Loan: the principal amount of the loan made or to be made by the Lender to th**e Borrower under this Agreement or (as the context requires) the principal amount outstanding for the time being of that loan.  
 
-Facility
+2. Interpretation
+--------------
+2.1 In this Agreement:
+2.1.1 unless the context otherwise requires, words in the singular shall include the plural and in the plural shall include the singular;  
+2.1.2 unless the context otherwise requires, a reference to one gender shall include a reference to the other genders; 
+2.1.3 a reference to a party shall include that party's successors, permitted assigns and permitted transferees;
+2.1.4 a reference to a statute or statutory provision is a reference to it as amended, extended or re-enacted from time to time;
+2.1.5 a reference to writing  or written includes mail, fax and e-mail; 
+2.1.6 unless the context otherwise requires, a reference to a clause or Schedule is to a clause of, or Schedule to, this Agreement; and
+2.1.7 any words following the terms including, include, in particular, for example  or any similar expression shall be construed as illustrative and shall not limit the sense of the words, description, definition, phrase or term preceding those terms.
+
+2.2 The Schedules are written in the javascript, and shall form part of this Agreement and shall have effect as if set out in full in the body of this Agreement. Any reference to this Agreement includes the Schedules. 
+
+2.3 In case of any inconsistency between the body of the Agreement and the Schedules, the body of the Agreement shall prevail, in which case the parties may amend the Schedules in good faith to reflect the body of the Agreement. 
+
+2.4 The explanatory notes to the Schedules do [NOT] form part of the Schedules or this Agreement and are provided by the parties in good faith for reference purpose.
+
+3. Facility
 --------
-The Lender grants to the Borrower an unsecured term  loan facility subject to the conditions, of this Agreement.
+3.1 The Lender grants to the Borrower an unsecured term  loan facility subject to the conditions, of this Agreement.
 
-Purpose
+4. Purpose
 -------
-
 4.1 The Borrower shall use all money borrowed under this Agreement for the sole purpose of expanding its business.  
 4.2 The Lender is not obliged to monitor or verify how any amount advanced under this Agreement is used.
 
-Commencement
+5. Commencement
 ------------
-This Agreement shall be deemed to have commenced on the date of this Agreement.
+5.1 This Agreement shall be deemed to have commenced on the date of this Agreement.
 
-Interest
+6. Interest
 --------
 
-6.1 The Borrower shall pay interest on the Loan as specified in the Schedule of this Agreement  
-6.2 If the Borrower fails to make any payment due under this Agreement on the due date for payment, interest on the unpaid amount shall accrue daily, from the date of non-payment to the date of actual payment (both before and after judgment), at 5% above the rate specified in clause 5.1.
+6.1 The Borrower shall pay interest on the Loan at the rate of [10]% per annum.
+6.2 Interest (a) shall accrue daily, (b) shall be payable monthly, in arrear, in accordance with Schedule [A] of this Agreement, and (c) shall be paid in bitcoins based on the BTC Exchange Rate.   
+6.3 If the Borrower fails to make any payment due under this Agreement on the due date for payment, interest on the unpaid amount shall accrue daily, from the date of non-payment to the date of actual payment (both before and after judgment), at 5% above the rate specified in clause 6.1 [as calculated in accordance with Schedule [C]]. 
 
-Costs
+7. Costs
 -----
 7.1 The Lender shall bear all costs and expenses (together with any value added tax on them) that the Lender incurs in connection with the negotiation and preservation and enforcement of the Loan and/or this Agreement.  
 7.2 The Borrower shall pay any stamp, documentary and other similar duties and taxes (if any) to which this Agreement may be subject, or give rise and shall indemnify the Lender against any losses or liabilities that it may incur as a result of any delay or omission by the Borrower in paying any such duties or taxes.
 
-Repayment
+8. Repayment
 ---------
-8.1 The Borrower shall repay the Loan as specified in the attached schedule.  
+8.1 The Borrower shall repay the Loan as specified in the attached Schedule [B].  
 8.2 All payments made by the Borrower under this Agreement shall be made in full, without set-off, counterclaim or condition, and free and clear of, and without any deduction or withholding.
 
-Representations, Warranties and Undertakings
+9. Representations, Warranties and Undertakings
 --------------------------------------------
-9.1The Borrower represents, warrants and undertakes to the Lender on the date of this Agreement:  
+9.1 The Borrower represents, warrants and undertakes to the Lender on the date of this Agreement:  
 (a) is a duly incorporated limited liability company validly existing under the laws of its jurisdiction of incorporation;  
 (b) has the power to enter into, deliver and perform, and has taken all necessary action to authorise its entry into, delivery and performance of, this Agreement; and  
 (c) has obtained all required authorisations to enable it to enter into, exercise its rights and comply with its obligations in this Agreement.  
+
 9.2 The entry into and performance by it of, and the transactions contemplated by, this Agreement, do not and will not contravene or conflict with:
 (a) its constitutional documents;  
 (b) any agreement or instrument binding on it or its assets or constitute a default or termination event (however described) under any such agreement or instrument; or  
 (c) any law or regulation or judicial or official order, applicable to it.
+
 9.3 The information, in written or electronic format, supplied by, or on its behalf, to the Lender in connection with this Agreement (including but not limited to the documents set out in Schedule 1) was, at the time it was/will be supplied or at the date it was/will be stated to be given (as the case may be):  
 (a) if it was factual information, complete, true and accurate in all material respects;  
 (b) if it was a financial projection or forecast, prepared on the basis of recent historical information and on the basis of reasonable assumptions and was fair and made on reasonable grounds; and  
 (c) if it was an opinion or intention, made after careful consideration and was fair and made on reasonable grounds; and  
 (d) not misleading in any material respect, nor rendered misleading by a failure to disclose other information.
 
-Events of Default
+10. Events of Default
 -----------------
 10.1 Each of the events or circumstances set out in this clause is an Event of Default.  
 (i) The Borrower fails to pay any sum payable by it under this Agreement.  
@@ -112,50 +121,31 @@ Events of Default
 (iv) The Borrower suspends or ceases to carry on (or threatens to suspend or cease to carry on) all or a substantial part of its business.  
 (v) The passing of a resolution for the winding up of the Borrower; or the appointment of a receiver, administrator or administrative receiver over the whole or any part of the assets of the Borrower or the making of any arrangement with the creditors of the Borrower for the affairs, business and property of the Borrower to be managed by a supervisor.
 
-Amendments, Waivers and Consents and Remedies
+11. Amendments, Waivers and Consents and Remedies
 ---------------------------------------------
 11.1 No amendment of this Agreement shall be effective unless it is in writing and signed by, or on behalf of, each party to it (or its authorised representative).  
 11.2 A waiver of any right or remedy under this Agreement or by law, or any consent given under this Agreement, is only effective if given in writing by the waiving or consenting party and shall not be deemed a waiver of any other breach or default. It only applies in the circumstances for which it is given and shall not prevent the party giving it from subsequently relying on the relevant provision.  
 11.3 A failure or delay by a party to exercise any right or remedy provided under this Agreement or by law shall not constitute a waiver of that or any other right or remedy, prevent or restrict any further exercise of that or any other right or remedy or constitute an election to affirm this Agreement. No election to affirm this Agreement by the Lender shall be effective unless it is in writing. 
 11.4 The rights and remedies provided under this Agreement are cumulative and are in addition to, and not exclusive of, any rights and remedies provided by law..  
 
-
-Assignment and transfer
+12. Assignment and transfer
 -----------------------
-13.1 The Lender may assign any of its rights under this Agreement or transfer all its rights or obligations by novation.  
-13.2 The Borrower may not assign any of its rights or transfer any of its rights or obligations under this Agreement.  
+12.1 The Lender may assign any of its rights under this Agreement or transfer all its rights or obligations by novation.  
+12.2 The Borrower may not assign any of its rights or transfer any of its rights or obligations under this Agreement.  
 
-Counterparts
+13. Counterparts
 ------------
-14.1 This Agreement may be executed in any number of counterparts, each of which when executed shall constitute a duplicate original, but all the counterparts shall together constitute one agreement.  
-14.2 No counterpart shall be effective until each party has executed at least one counterpart.  
+13.1 This Agreement may be executed in any number of counterparts, each of which when executed shall constitute a duplicate original, but all the counterparts shall together constitute one agreement.  
+13.2 No counterpart shall be effective until each party has executed at least one counterpart.  
 
-Severance
+14. Severance
 ---------
-If any provision (or part of a provision) of this Agreement is or
-becomes invalid, illegal or unenforceable, it shall be deemed modified
-to the minimum extent necessary to make it valid, legal and
-enforceable. If such modification is not possible, the relevant
-provision (or part of a provision) shall be deemed deleted. Any
-modification to or deletion of a provision (or part of a provision)
-under this clause shall not affect the legality, validity and
-enforceability of the rest of this Agreement.
+14.1 If any provision (or part of a provision) of this Agreement is or becomes invalid, illegal or unenforceable, it shall be deemed modified
+to the minimum extent necessary to make it valid, legal and enforceable. If such modification is not possible, the relevant
+provision (or part of a provision) shall be deemed deleted. Any modification to or deletion of a provision (or part of a provision)
+under this clause shall not affect the legality, validity and enforceability of the rest of this Agreement.
 
-
-Interpretation
---------------
-  In this Agreement:
-    unless the context otherwise requires, words in the singular shall include the plural and in the plural shall include the singular;  
-    unless the context otherwise requires, a reference to one gender shall include a reference to the other genders; 
-    a reference to a party shall include that party's successors, permitted assigns and permitted transferees;
-    a reference to a statute or statutory provision is a reference to it as amended, extended or re-enacted from time to time;
-    a reference to writing  or written includes mail, fax and e-mail; 
-    unless the context otherwise requires, a reference to a clause or Schedule is to a clause of, or Schedule to, this Agreement; and
-    any words following the terms including, include, in particular, for example  or any similar expression shall be construed as illustrative and shall not limit the sense of the words, description, definition, phrase or term preceding those terms.
-    The Schedule forms part of this Agreement and shall have effect as if set out in full in the body of this Agreement. Any reference to this Agreement includes the Schedule.
-
-
-Notices
+15. Notices
 -------
 15.1 Any notice or other communication given to a party under or in connection with this Agreement shall be:
 (a) in writing;
@@ -185,6 +175,12 @@ party to the other from time to time.
 15.3 A notice or other communication given on a day that is not a Business Day, or after normal business hours, in the place it is received, shall be deemed to have been received on the next Business Day.
 
 15.4 Any notice or other communication given to the Lender shall be deemed to have been received only on actual receipt.
+
+16.Governing law and jurisdiction
+------------------------------
+16.1 This Agreement and any dispute or claim arising out of or in connection with it or its subject matter or formation (including non-contractual disputes or claims) shall be governed by and construed in accordance with the law of Hong Kong.  
+
+16.2 Each party irrevocably agrees that, subject as provided below, the courts of Hong Kong shall have non-exclusive jurisdiction over any dispute or claim that arises out of, or in connection with this Agreement or its subject matter or formation (including non-contractual disputes or claims). Nothing in this clause shall limit the right of the Lender to take proceedings against the Borrower in any other court of competent jurisdiction, nor shall the taking of proceedings in any one or more jurisdictions preclude the taking of proceedings in any other jurisdictions, whether concurrently or not, to the extent permitted by the law of such other jurisdiction.
 
 This Agreement has been entered into on the date stated at the beginning of it.
 
