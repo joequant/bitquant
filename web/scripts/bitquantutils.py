8ded9287fe8f27de4f620c628914d80fa5b37563
Joseph C Wang <joequant@gmail.com>
2015-03-24 20:03:14 +0800

    bitquantutils fixes

--- web/scripts/bitquantutils.py
+++ web/scripts/bitquantutils.py
@@ -1,3 +1,6 @@
+import tornado.ioloop
+has_ioloop = tornado.ioloop.IOLoop.initialized()
+
 def register_port(prefix, port):
     import json
     import urllib.request
@@ -11,25 +14,22 @@ def register_tornado_handler(prefix, port, handler):
     import tornado.ioloop
     import tornado.web
     register_port(prefix, port)
-    has_ioloop = tornado.ioloop.IOLoop.initialized()
     application = tornado.web.Application([
-        (prefix, handler),
+        (prefix + ".*", handler),
         ])
     application.listen(port)
-    if not has_ioloop:
-        tornado.ioloop.IOLoop.instance().start()
 
 def register_wsgi(prefix, port, handler):
     import tornado.ioloop
     import tornado.wsgi
     import tornado.web
     register_port(prefix, port)
-    has_ioloop = tornado.ioloop.IOLoop.initialized()
     container = tornado.wsgi.WSGIContainer(handler)
     application = tornado.web.Application([
-        (prefix, tornado.web.FallbackHandler, dict(fallback=container))
+        (prefix + ".*", tornado.web.FallbackHandler, dict(fallback=container))
         ])
     application.listen(port)
+
+def start_loop():
     if not has_ioloop:
         tornado.ioloop.IOLoop.instance().start()
-    
