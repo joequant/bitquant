9aa881b8d1c91048ced0c59aaa62ab325ae8edb3
Joseph C Wang <joequant@gmail.com>
2015-04-02 22:44:31 +0800

    replace with pyswagger

--- web/scripts/install-python-sudo.sh
+++ web/scripts/install-python-sudo.sh
@@ -4,6 +4,18 @@
 echo "Running python installation"
 ROOT_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 . $ROOT_SCRIPT_DIR/rootcheck.sh
+SCRIPT_DIR=$1
+ME=$2
 
-
-pip3 install swaggerpy
+# put yelp_uri in back to override downloaded version
+for repo in pyswagger ;
+do
+echo /home/$ME/git/$repo
+if [ -d /home/$ME/git/$repo ] ; then
+pushd /home/$ME/git/$repo > /dev/null
+python3 setup.py clean
+python3 setup.py install --force
+python3 setup.py clean
+popd > /dev/null
+fi
+done
--- web/scripts/install-python.sh
+++ web/scripts/install-python.sh
@@ -3,11 +3,13 @@
 
 echo "Running python installation"
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+ME=`stat -c "%U" $SCRIPT_DIR/install-r-pkgs.sh`
 
 pushd $SCRIPT_DIR > /dev/null
 . norootcheck.sh
 sudo /usr/share/bitquant/install-python-sudo.sh $SCRIPT_DIR $ME
-popd
+popd > /dev/null
+
 
 
 
