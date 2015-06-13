a002f8bd395c698434088e0bcb6a3aa330afc6d3
Joseph C Wang <joequant@gmail.com>
2015-06-13 08:25:46 +0800

    subclass

---
+++ smart-contracts/models/subclass/Notes.js
@@ -0,0 +1,12 @@
+// Copyright (c) 2014, Bitquant Research Laboratories (Asia) Ltd.
+// Licensed under the Simplified BSD License
+"use strict";
+
+
+if (typeof define !== 'function') {
+    var define = require('amdefine')(module);
+}
+
+define (["../loan/Notes"], function(Notes) {
+    return Notes;
+});
---
+++ smart-contracts/models/subclass/TermSheet.js
@@ -0,0 +1,13 @@
+// Copyright (c) 2014, Bitquant Research Laboratories (Asia) Ltd.
+// Licensed under the Simplified BSD License
+
+"use strict";
+
+if (typeof define !== 'function') {
+    var define = require('amdefine')(module);
+}
+
+
+define(["../loan/TermSheet"], function(TermSheet) {
+    return TermSheet;
+});
