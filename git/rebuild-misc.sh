8dc8c1544b43d35eefa7a8e724a307550d9ac940
Joseph C Wang <joequant@gmail.com>
2015-07-18 17:52:11 +0800

    don't rebuild node for shiny-server

--- git/rebuild-misc.sh
+++ git/rebuild-misc.sh
@@ -22,9 +22,15 @@ echo "gyp is installed.  compile will fail.  please uninstall"]
 exit 1
 fi
 pushd ../../shiny-server > /dev/null
-cmake -DCMAKE_INSTALL_PREFIX="/usr/lib" -DPYTHON="/usr/bin/python2"
+git checkout .
+sed -i -e "s!bin/node!!" -e "s!bin/npm!!" CMakeLists.txt
+sed -i -e "s!add_subdirectory(external/node)!!g" CMakeLists.txt
+sed -i -e "s!add_subdirectory(external/pandoc)!!g" CMakeLists.txt
+cmake -DCMAKE_INSTALL_PREFIX="/usr/lib" -DPYTHON="/usr/bin/python2" .
 make
 npm --python="$PYTHON" rebuild
-ext/node/lib/node_modules/npm/node_modules/node-gyp/bin/node-gyp.js --python="$PYTHON" rebuild
+npm --python="$PYTHON" install node-gyp
+node-gyp --python="$PYTHON" rebuild
+git checkout .
 popd > /dev/null
 fi
