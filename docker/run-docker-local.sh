b8d1e548b9fe9fd1a4a4c53eba10b2624bfc23b0
Joseph C Wang <joequant@gmail.com>
2015-07-27 03:39:04 +0800

    run docker with local store

---
+++ docker/run-docker-local.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+IMAGE=bitstation
+if [ "$1" != "" ] ; then
+   IMAGE=$1
+fi
+
+if [ ! -e ~/volumes/bitstation ] ; then
+mkdir -p ~/volumes/bitstation
+pushd ~/volumes/bitstation
+sudo systemctl stop httpd
+id=$(sudo docker create $IMAGE)
+sudo docker cp $id:/home - | tar xf -
+mkdir -p var/lib
+pushd var/lib
+sudo docker cp $id:/var/lib/dokuwiki - | tar xf -
+sudo docker run \
+-v ~/volumes/bitstation/var/lib/dokuwiki:/var/lib/dokuwiki \
+$IMAGE \
+chown -R apache:apache /var/lib/dokuwiki
+popd
+sudo docker rm -v $id
+popd
+fi
+
+sudo docker run --privileged -v /sys/fs/cgroup:/sys/fs/cgroup:ro \
+-v ~/volumes/bitstation/home:/home \
+-v ~/volumes/bitstation/var/lib/dokuwiki:/var/lib/dokuwiki \
+-p 80:80 -p 443:443 $IMAGE
