6f7b9763789e1271009322ac73bc89363d6a580a
Joseph C Wang <joequant@gmail.com>
2015-08-08 04:07:35 +0800

    add python drivers

--- web/scripts/install-python-sudo.sh
+++ web/scripts/install-python-sudo.sh
@@ -7,8 +7,11 @@ ROOT_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 SCRIPT_DIR=$1
 ME=$2
 
+#missing numba, blaze for now
+
 for packages in zipline vispy pyalgotrade statsmodels quandl \
 patsy beautifulsoup4 pymongo sympy ipython_mongo seaborn \
-toyplot ad collections-extended TA-Lib ;
+toyplot ad collections-extended TA-Lib mpmath multimethods \
+openpyxl param xlrd xlwt xlutils xray ;
 do pip3 install --upgrade $packages ;
 done
