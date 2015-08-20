7c103bd93a6861bb099a2234451e63d55bae620b
Joseph C Wang <joequant@gmail.com>
2015-08-21 01:22:33 +0800

    add holoviews

--- web/scripts/install-python-sudo.sh
+++ web/scripts/install-python-sudo.sh
@@ -14,6 +14,6 @@ for packages in zipline vispy pyalgotrade statsmodels quandl \
 patsy beautifulsoup4 pymongo sympy ipython_mongo seaborn \
 toyplot ad collections-extended TA-Lib mpmath multimethods \
 openpyxl param xray FinDates html5lib twilio plivo ggplot pygal \
-plotly ;
+plotly holoviews ;
 do pip3 install $PYTHON_ARGS $packages ;
 done
