f375b7cc22d54cb583ea9ace412b41f473da2145
Joseph C Wang <joequant@gmail.com>
2015-08-20 23:10:22 +0800

    add more plotting packages

--- web/scripts/install-python-sudo.sh
+++ web/scripts/install-python-sudo.sh
@@ -13,6 +13,7 @@ ME=$2
 for packages in zipline vispy pyalgotrade statsmodels quandl \
 patsy beautifulsoup4 pymongo sympy ipython_mongo seaborn \
 toyplot ad collections-extended TA-Lib mpmath multimethods \
-openpyxl param xray FinDates html5lib twilio plivo ;
+openpyxl param xray FinDates html5lib twilio plivo ggplot pygal \
+plotly ;
 do pip3 install $PYTHON_ARGS $packages ;
 done
