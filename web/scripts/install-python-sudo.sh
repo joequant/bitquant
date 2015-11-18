b0dc672738605599d5e5bdf11ec0264f1e2d07d4
Joseph C Wang <joequant@gmail.com>
2015-11-18 22:51:58 +0800

    remove zipline for now

--- web/scripts/install-python-sudo.sh
+++ web/scripts/install-python-sudo.sh
@@ -9,6 +9,7 @@ ME=$2
 
 #PYTHON_ARGS=--upgrade
 #missing numba for now
+#missing zipline since requirements installation causes issues
 
 for packages in \
     vispy pyalgotrade statsmodels quandl \
@@ -17,7 +18,7 @@ for packages in \
 	    openpyxl param xray FinDates html5lib twilio plivo ggplot pygal \
 	    plotly holoviews ipython[notebook] bokeh jupyterhub \
 	    fastcluster  algobroker ib-api pandas-datareader \
-	    ethercalc-python blaze statsmodels zipline ;
+	    ethercalc-python blaze statsmodels ;
 do pip3 install $PYTHON_ARGS $packages ;
 done
 
