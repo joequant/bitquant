ebfb16e4e93c64480da4e4c5fd3eecf7db5064ad
Joseph C Wang <joequant@gmail.com>
2015-12-03 23:47:25 +0800

    add body to install ethercalc

--- web/scripts/install-ethercalc.py
+++ web/scripts/install-ethercalc.py
@@ -3,12 +3,20 @@
 import ethercalc
 import os
 import sys
+import requests
 
 cwd = os.path.dirname(os.path.realpath(sys.argv[0]))
 ethercalc_dir = os.path.join(cwd, "ethercalc")
+ec = ethercalc.EtherCalc("http://localhost/calc/")
 for i in os.listdir(ethercalc_dir):
     if i.endswith(".clc"):
         item = i.rsplit(".", maxsplit=1)[0]
-        print(item)
-    
+        try:
+            a = ec.export(i)
+        except requests.exceptions.HTTPError:
+            txt = open(os.path.join(ethercalc_dir, i))
+            data = txt.read()
+            print (data)
+#            print (ec.update(item, data))
+
 
