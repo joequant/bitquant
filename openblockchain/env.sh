5fad40c32d286d8708d8900e5e2157bfbd4efafb
Joseph C Wang <joequant@gmail.com>
2016-03-21 03:59:53 +0800

    env file tweaks

--- openblockchain/env.sh
+++ openblockchain/env.sh
@@ -1,2 +1,4 @@
-export GOPATH=$HOME/golang
+export SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+export GOPATH=$SCRIPT_DIR/golang
+export DOCKER_STORAGE_BACKEND=btrfs
 #export USE_LOCAL_OBC_BASEIMAGE=1
