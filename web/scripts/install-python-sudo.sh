12639c2b0a0fa1c98c289f4062740378c1a4a5c7
Joseph C Wang <joequant@gmail.com>
2016-03-25 00:36:22 +0800

    upgrade pip3 and bitcoin-price-api

--- web/scripts/install-python-sudo.sh
+++ web/scripts/install-python-sudo.sh
@@ -11,6 +11,7 @@ ME=$2
 #missing numba for now
 #missing zipline since requirements installation causes issues
 
+pip3 install --upgrade pip
 for packages in \
     vispy pyalgotrade statsmodels quandl \
 	    patsy beautifulsoup4 pymongo ipython_mongo seaborn \
@@ -23,6 +24,7 @@ for packages in \
 	    git+https://github.com/joequant/ethercalc-python.git \
             git+https://github.com/joequant/spyre.git \
 	    git+https://github.com/joequant/algobroker.git \
+	    git+https://github.com/joequant/bitcoin-price-api.git \
 	    git+https://github.com/joequant/matta.git \
             configproxy prettyplotlib mpld3 networkx \
             iminuit lmfit redis_kernel bash_kernel ; 
