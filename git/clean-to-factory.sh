aa4c4d0fc0d11e8b49c90b9e93cda00b16429955
Joseph C Wang <joequant@gmail.com>
2016-04-17 05:33:34 +0800

    remove obsolete scripts

--- git/clean-to-factory.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-MY_NAME=joequant
-cd $SCRIPT_DIR
-
-pushd ../..
-rm -rf ethercalc OG-Platform OG-PlatformNative \
-   OG-Tools quantlib bitquant/web/log/bootstrap.done
-popd
