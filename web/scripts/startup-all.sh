8753c4f99fe733b008baa788be90b6bb7b55eabc
Joseph C Wang <joequant@gmail.com>
2016-04-29 21:47:15 +0800

    redis has moved to /usr/bin

--- web/scripts/startup-all.sh
+++ web/scripts/startup-all.sh
@@ -6,7 +6,7 @@ LOG_DIR=$WEB_DIR/log
 cd $SCRIPT_DIR
 . rootcheck.sh
 
-su redis -s "/bin/bash" -c "/usr/sbin/redis-server /etc/redis.conf" &
+su redis -s "/bin/bash" -c "/usr/bin/redis-server /etc/redis.conf" &
 su mongod -s "/bin/bash" -c "/usr/bin/mongod --quiet -f /etc/mongod.conf" &
 /usr/sbin/httpd -DFOREGROUND &
 
