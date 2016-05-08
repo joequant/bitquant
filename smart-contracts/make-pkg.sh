df0dc96cb8b3a6295ba6ab265fadfee707d62368
Joseph C Wang <joequant@gmail.com>
2016-05-08 20:27:33 +0800

    update execution sheet

--- smart-contracts/make-pkg.sh
+++ smart-contracts/make-pkg.sh
@@ -1,33 +1,36 @@
 #!/bin/bash
-
+set -v
+set -e
 ITEM=$1
-DATE=`date +'%Y%m%d'`
-DIR=$ITEM-$DATE
+DATE=$(date +'%Y%m%d')
+ZIPDIR=$(basename $ITEM)-$DATE
+ITEMDIR=$(dirname $ITEM)
 
-if [ -e ~/$DIR.zip ] ; then
-    echo "package already exists.  ending"
+if [ -e ~/$ZIPDIR.zip ] ; then
+    echo "package ~/$ZIPDIR.zip already exists.  ending"
     exit 1
 fi
 
-if [ -e ~/$DIR ] ; then
-    mv ~/$DIR ~/$DIR.old
+if [ -e ~/$ZIPDIR ] ; then
+    mv ~/$ZIPDIR ~/$ZIPDIR.old
 fi
 
-mkdir ~/$DIR
-cp *.sh *.js *.css *.md *.html *.txt ~/$DIR
-cp -r node_modules ~/$DIR
+mkdir ~/$ZIPDIR
+cp *.sh *.js *.css *.md *.html ~/$ZIPDIR 
+cp -r node_modules ~/$ZIPDIR
 rm -f $ITEM/*~
-cp -r $ITEM ~/$DIR
-sed -i -e s!models/loan!$ITEM! ~/$DIR/contract_viewer.html
-sed -i -e s!models/loan!$ITEM! ~/$DIR/calc-contract.js
+mkdir -p ~/$ZIPDIR/$ITEMDIR
+cp -r $ITEM ~/$ZIPDIR/$ITEMDIR
+sed -i -e s!models/loan!$ITEM! ~/$ZIPDIR/contract_viewer.html
+sed -i -e s!models/loan!$ITEM! ~/$ZIPDIR/calc-contract.js
 
 cd ~
-zip -r ~/$DIR $DIR
+zip -r ~/$ZIPDIR $ZIPDIR
 echo
-pushd ~/$DIR/$ITEM > /dev/null
+pushd ~/$ZIPDIR/$ITEM > /dev/null
 for i in *.js ; do
     echo `md5sum $i`
 done
 popd > /dev/null
-rm -rf $DIR
+rm -rf $ZIPDIR
 
