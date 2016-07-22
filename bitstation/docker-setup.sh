3307890bd87d1f0463ee154a0273d4bf24c502ba
Joseph C Wang <joequant@gmail.com>
2016-07-22 22:46:47 +0800

    try ignoring error code

--- bitstation/docker-setup.sh
+++ bitstation/docker-setup.sh
@@ -45,10 +45,9 @@ rm -f /lib/systemd/system/*journal*
 rm -f /lib/systemd/system/*networkd*
 rm -rf /etc/resolveconf /sbin/resolvconf /etc/rc.d/init.d/resolvconf
 
-URPMI_OPTIONS="--excludedocs --no-md5sum --no-verify-rpm" 
-   /usr/share/bitquant/install-build-deps.sh 
-su user -c "~user/git/bitquant/web/scripts/bootstrap.sh" 
-su user -c "touch ~user/git/bitquant/web/log/bootstrap.done"
+URPMI_OPTIONS="--excludedocs --no-md5sum --no-verify-rpm" /usr/share/bitquant/install-build-deps.sh 
+su user -c "~user/git/bitquant/web/scripts/bootstrap.sh" || true 
+su user -c "touch ~user/git/bitquant/web/log/bootstrap.done" || true
 ~user/git/bitquant/web/scripts/remove-build-deps.sh
 rm -rf /usr/lib64/python*/test
 rm -rf /usr/lib64/python*/site-packages/pandas/tests
