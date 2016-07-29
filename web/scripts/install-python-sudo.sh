7da763df0a5ff43547004cc2cad28636d48fa750
Joseph C Wang <joequant@gmail.com>
2016-07-29 17:37:54 +0800

    add quantopian

--- web/scripts/install-python-sudo.sh
+++ web/scripts/install-python-sudo.sh
@@ -33,7 +33,8 @@ for packages in \
 	    git+https://github.com/joequant/bitcoin-price-api.git \
             git+https://github.com/joequant/datagramas.git \
 	    git+https://github.com/joequant/pythalesians.git \
-            configproxy prettyplotlib mpld3 networkx \
+	    git+https://github.com/quantopian/pyfolio.git \
+            configproxy prettyplotlib mpld3 networkx qgrid \
 	    git+https://github.com/joequant/IPython-notebook-extensions.git \
             iminuit lmfit redis_kernel bash_kernel \
             caravel ; 
