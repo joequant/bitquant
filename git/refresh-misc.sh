74d96d795b1fdbd0a8f7f5be8484066b43ec34f5
Joseph C Wang <joequant@gmail.com>
2016-11-01 12:49:50 +0800

    refactor refresh

--- git/refresh-misc.sh
+++ git/refresh-misc.sh
@@ -1,53 +1,5 @@
 #!/bin/bash
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $SCRIPT_DIR
-. repos-info.sh
-REPOS_SET="misc"
-UPLOAD=0
+./refresh-git.sh misc
 
-for i in "$@"
-do
-    case $i in
-	--upload)
-	    UPLOAD=1
-	    shift
-	    ;;
-	*)
-	    ;;
-    esac
-done
-
-if [ $# -gt 0 ]; then
-   echo "$1"
-   REPOS_SET=$1
-fi
-
-git config --global credential.helper cache
-
-pushd ../.. > /dev/null
-for repo in ${repos[$REPOS_SET]} ; do
-    if [ -d "$repo" ]; then
-	pushd $repo > /dev/null
-	echo "Processing $repo"
-	if [[ ${branch[$repo]} ]] ; then
-	    mybranch=${branch[$repo]}
-	else
-	    mybranch="master"
-	fi
-	git branch $mybranch origin/$mybranch
-	git checkout $mybranch
-	if [[ ${upstream[$repo]} ]] ; then
-	    git fetch upstream
-	fi
-	git fetch origin
-	git stash
-	if [[ ${upstream[$repo]} ]] ; then
-	    git rebase upstream/$mybranch
-	    if [ $UPLOAD -eq 1 ] ; then
-		git push --set-upstream origin $mybranch
-	    fi
-	fi
-	popd > /dev/null
-    fi
-done
-popd > /dev/null
--- git/refresh-quantlib.sh
+++ git/refresh-quantlib.sh
@@ -2,25 +2,4 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 MY_NAME=joequant
 cd $SCRIPT_DIR
-if [ "$1" == "--upload" ] ; then
-export UPLOAD=1
-else
-export UPLOAD=0
-fi
-
-
-echo "Refreshing quantlib"
-pushd ../.. > /dev/null
-pushd quantlib > /dev/null
-git fetch upstream
-git fetch origin
-git stash
-git branch master origin/master
-git checkout master
-git rebase upstream/master
-git checkout bitcoin
-git rebase master
-if [ $UPLOAD -eq 1 ] ; then git push --set-upstream origin master ; fi
-popd > /dev/null
-popd > /dev/null
-
+./refresh-git.sh quantlib
