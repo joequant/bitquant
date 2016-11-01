81368797788270f3b70cd55d8fc33248e16b0435
Joseph C Wang <joequant@gmail.com>
2016-11-01 12:43:10 +0800

    refactor with new repo array

--- git/refresh-misc.sh
+++ git/refresh-misc.sh
@@ -2,35 +2,52 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $SCRIPT_DIR
 . repos-info.sh
-if [ "$1" == "--upload" ] ; then
-export UPLOAD=1
-else
-export UPLOAD=0
+REPOS_SET="misc"
+UPLOAD=0
+
+for i in "$@"
+do
+    case $i in
+	--upload)
+	    UPLOAD=1
+	    shift
+	    ;;
+	*)
+	    ;;
+    esac
+done
+
+if [ $# -gt 0 ]; then
+   echo "$1"
+   REPOS_SET=$1
 fi
+
 git config --global credential.helper cache
 
 pushd ../.. > /dev/null
-for repo in $repos_misc ; do
-if [ -d "$repo" ]
-then
-pushd $repo > /dev/null
-echo "Processing $repo"
-if [[ ${branch[$repo]} ]]
-then mybranch=${branch[$repo]}
-else mybranch="master"
-fi
-git branch $mybranch origin/$mybranch
-git checkout $mybranch
-if [[ ${upstream[$repo]} ]] ; then
-git fetch upstream
-fi
-git fetch origin
-git stash
-if [[ ${upstream[$repo]} ]] ; then
-git rebase upstream/$mybranch
-if [ $UPLOAD -eq 1 ] ; then git push --set-upstream origin $mybranch ; fi
-fi
-popd > /dev/null
-fi
+for repo in ${repos[$REPOS_SET]} ; do
+    if [ -d "$repo" ]; then
+	pushd $repo > /dev/null
+	echo "Processing $repo"
+	if [[ ${branch[$repo]} ]] ; then
+	    mybranch=${branch[$repo]}
+	else
+	    mybranch="master"
+	fi
+	git branch $mybranch origin/$mybranch
+	git checkout $mybranch
+	if [[ ${upstream[$repo]} ]] ; then
+	    git fetch upstream
+	fi
+	git fetch origin
+	git stash
+	if [[ ${upstream[$repo]} ]] ; then
+	    git rebase upstream/$mybranch
+	    if [ $UPLOAD -eq 1 ] ; then
+		git push --set-upstream origin $mybranch
+	    fi
+	fi
+	popd > /dev/null
+    fi
 done
 popd > /dev/null
