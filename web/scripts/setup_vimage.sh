95c5b12a6f1b7f1e50d66a4d2bd426417db4d82a
Joseph C Wang <joequant@gmail.com>
2017-09-27 03:43:32 +0800

    convert from urpmi to dnf

--- web/scripts/setup_vimage.sh
+++ web/scripts/setup_vimage.sh
@@ -23,36 +23,7 @@ URPMI_ROOT=http://mirrors.kernel.org/mageia/distrib
 cd /home/$USER
 chown -R $USER":"$USER git
 
-echo "Resetting urpmi"
-urpmi.removemedia -a
-urpmi.addmedia --distrib --mirrorlist 'http://mirrors.mageia.org/api/mageia.'$VERSION'.'`uname -m`'.list'
-
-urpmi.update --ignore "Core 32bit Release" "Core 32bit Updates"
-urpmi.update --no-ignore "Nonfree Release" "Nonfree Updates"
-urpmi.update --no-ignore "Tainted Release" "Tainted Updates" 
-if $TESTING ; then
-    urpmi.update --no-ignore "Core Updates" "Core Updates Testing"
-    urpmi.update --no-ignore "Core Backports" "Core Backports Testing"
-fi
-
-# Add backup server to make sure that we get fresh rpms
-if [ "$VERSION" = "cauldron" ] ; then
-urpmi.addmedia "Core backup" $URPMI_ROOT/$VERSION/`uname -m`/media/core/release --no-md5sum
-urpmi.addmedia "Core updates backup" $URPMI_ROOT/$VERSION/`uname -m`/media/core/updates --no-md5sum
-urpmi.addmedia "Nonfree backup" $URPMI_ROOT/$VERSION/`uname -m`/media/nonfree/release --no-md5sum
-urpmi.addmedia "Nonfree updates backup" $URPMI_ROOT/$VERSION/`uname -m`/media/nonfree/updates --no-md5sum
-urpmi.addmedia "Tainted backup" $URPMI_ROOT/$VERSION/`uname -m`/media/tainted/release --no-md5sum
-urpmi.addmedia "Tainted updates backup" $URPMI_ROOT/$VERSION/`uname -m`/media/tainted/updates --no-md5sum
-
-if $TESTING ; then
-urpmi.addmedia "Core updates testing backup" $URPMI_ROOT/$VERSION/`uname -m`/media/core/updates_testing
-fi
-urpmi.addmedia "Core backports backup" $URPMI_ROOT/$VERSION/`uname -m`/media/core/backports
-urpmi.addmedia "Backports testing backup" $URPMI_ROOT/$VERSION/`uname -m`/media/core/backports_testing
-fi
-
-urpmi.update -a
-#urpme --force --auto-orphans
+dnf --setopt=install_weak_deps=False --best -v -y --nodocs upgrade 
 
 echo "Copy configuration files"
 pushd $SCRIPT_DIR > /dev/null
