fdc99cc831125a5af6c101a3a7cc4d7984c63217
Joseph C Wang <joequant@gmail.com>
2017-10-16 16:28:16 +0800

    add startup comments

--- web/scripts/startup-all.sh
+++ web/scripts/startup-all.sh
@@ -6,8 +6,10 @@ LOG_DIR=$WEB_DIR/log
 cd $SCRIPT_DIR
 . rootcheck.sh
 
+echo "Start redis"
 su redis -s "/bin/bash" -c "/usr/bin/redis-server /etc/redis.conf" &
-chown mongod:mongod /var/lib/mongodb
+echo "Start mongo"
+chown -R mongod:mongod /var/lib/mongodb
 su mongod -s "/bin/bash" -c "/usr/bin/mongod --quiet -f /etc/mongod.conf" &
 /usr/sbin/httpd -DFOREGROUND &
 if [ -f /etc/webmin/start ] ; then
