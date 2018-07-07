6275146690e67cda9869de6162d7bab14bc871a9
Joseph C Wang <joequant@gmail.com>
2018-07-07 22:58:50 +0800

    parallelize python

--- web/scripts/install-python-sudo.sh
+++ web/scripts/install-python-sudo.sh
@@ -27,60 +27,137 @@ popd
 pip3 install --upgrade pip --prefix /usr
 # reinstall to get jupyter executable
 pip3 install --upgrade --force-reinstall jupyter-core --prefix /usr
+pip3 install --upgrade $PYTHON_ARGS entrypoints python-dateutil==2.6.1 --prefix /usr
+
 
 PYCURL_SSL_LIBRARY=openssl pip3 install pycurl --prefix /usr
 
-for packages in \
-    entrypoints \
-	python-dateutil==2.6.1 \
-    eventsourcing \
-    numpy \
-    ipympl \
-    matplotlib \
-    pandas \
-    scipy \
-    astropy \
-    jupyterhub \
-    nbconvert \
-    sudospawner \
-    circuits \
-    dask \
-    xarray \
-    networkx \
-	lightning-python \
-	vispy pyalgotrade statsmodels quandl ipywidgets ipyvolume \
-	jupyter_declarativewidgets pythreejs vega nbpresent \
-	jupyter_latex_envs jupyterlab_latex \
-	ipyleaflet bqplot cookiecutter pyquickhelper scikit-image \
-	    patsy beautifulsoup4 pymongo ipython_mongo seaborn ipysheet \
-	    toyplot ad collections-extended TA-Lib mpmath multimethods \
-	    openpyxl param xray FinDates html5lib twilio plivo ggplot pygal \
-	    plotly holoviews bokeh fastcluster ib-api pandas-datareader \
-	    blaze statsmodels redis redis-dump-load \
-	    git+https://github.com/joequant/ethercalc-python.git \
-            git+https://github.com/joequant/spyre.git \
-	    git+https://github.com/joequant/cryptoexchange.git \
-	    git+https://github.com/joequant/algobroker.git \
-	    git+https://github.com/joequant/bitcoin-price-api.git \
-	    git+https://github.com/joequant/pythalesians.git \
-	    git+https://github.com/quantopian/pyfolio.git \
-            configproxy prettyplotlib mpld3 networkx qgrid \
-	    git+https://github.com/joequant/IPython-notebook-extensions.git \
-            iminuit lmfit redis_kernel bash_kernel octave_kernel \
-	    jupyter_nbextensions_configurator pyfolio \
-            superset VisualPortfolio empyrical qfrm tradingWithPython \
-            trade pybacktest chinesestockapi bizdays \
-            git+https://github.com/bashtage/arch.git ffn \
-	    git+https://github.com/joequant/OrderBook.git \
-	    git+https://github.com/joequant/quantdsl.git \
-	    pulsar pyspark cvxopt \
-	    git+https://github.com/joequant/dynts.git \
-            pynance jupyterlab ipyleaflet keras mxnet nolearn \
-	    theano tensorflow nltk gensim scrapy statsmodels gym milk \
-	    neurolab pyrenn jupyterlab_widgets\
-	    jhub_remote_user_authenticator jupyterlab-discovery \
-	    dash dash_renderer dash_core_components dash_html_components ;
-do pip3 install --upgrade $PYTHON_ARGS $packages --prefix /usr ;
+cat <<EOF | xargs --max-args=1 --max-procs=$(nproc) pip3 install --upgrade $PYTHON_ARGS --prefix /usr
+eventsourcing
+numpy
+ipympl
+matplotlib
+pandas
+scipy
+astropy
+jupyterhub
+nbconvert
+sudospawner
+circuits
+dask
+xarray
+networkx
+lightning-python
+vispy 
+pyalgotrade 
+statsmodels 
+quandl
+ipywidgets 
+ipyvolume
+jupyter_declarativewidgets 
+pythreejs
+vega
+nbpresent
+jupyter_latex_envs
+jupyterlab_latex
+ipyleaflet
+bqplot
+cookiecutter
+pyquickhelper
+scikit-image
+patsy
+beautifulsoup4
+pymongo
+ipython_mongo
+seaborn
+ipysheet
+toyplot
+ad
+collections-extended
+TA-Lib
+mpmath
+multimethods
+openpyxl
+param
+xray
+FinDates
+html5lib
+twilio
+plivo
+ggplot
+pygal
+plotly
+holoviews
+bokeh
+fastcluster
+ib-api
+pandas-datareader
+blaze
+statsmodels
+redis
+redis-dump-load
+git+https://github.com/joequant/ethercalc-python.git
+git+https://github.com/joequant/spyre.git
+git+https://github.com/joequant/cryptoexchange.git
+git+https://github.com/joequant/algobroker.git
+git+https://github.com/joequant/bitcoin-price-api.git
+git+https://github.com/joequant/pythalesians.git
+git+https://github.com/quantopian/pyfolio.git
+configproxy
+prettyplotlib
+mpld3
+networkx
+qgrid
+git+https://github.com/joequant/IPython-notebook-extensions.git
+iminuit
+lmfit
+redis_kernel
+bash_kernel
+octave_kernel
+jupyter_nbextensions_configurator
+pyfolio
+superset
+VisualPortfolio
+empyrical
+qfrm
+tradingWithPython
+trade
+pybacktest
+chinesestockapi
+bizdays
+git+https://github.com/bashtage/arch.git
+ffn
+git+https://github.com/joequant/OrderBook.git
+git+https://github.com/joequant/quantdsl.git
+pulsar
+pyspark
+cvxopt
+git+https://github.com/joequant/dynts.git
+pynance
+jupyterlab
+ipyleaflet
+keras
+mxnet
+nolearn
+theano
+tensorflow
+nltk
+gensim
+scrapy
+statsmodels
+gym
+milk
+neurolab
+pyrenn
+jupyterlab_widgets
+jhub_remote_user_authenticator
+jupyterlab-discovery
+dash
+dash_renderer
+dash_core_components
+dash_html_components
+EOF
+do 
 done
 
 jupyter contrib nbextension install --sys-prefix
