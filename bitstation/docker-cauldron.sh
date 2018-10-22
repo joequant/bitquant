19db98ec1c389e721916a7319497d1344bc67d46
Joseph C Wang <joequant@gmail.com>
2018-10-23 02:03:34 +0800

    fix transaction

--- bitstation/docker-cauldron.sh
+++ bitstation/docker-cauldron.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 dnf shell -v -y  <<EOF
 install --setopt=install_weak_deps=False --nodocs --allowerasing --best 'dnf-command(config-manager)' mageia-repos-cauldron
-transaction run
+run
 repo disable mageia-x86_64
 repo disable updates-x86_64
 repo enable cauldron-x86_64
 repo enable cauldron-x86_64-nonfree
 repo enable cauldron-x86_64-tainted
 config-manager  --add-repo http://mirrors.kernel.org/mageia/distrib/cauldron/x86_64/media/core/release cauldron
-transaction run
+run
 upgrade --allowerasing --best --nodocs --setopt=install_weak_deps=False
 run
 EOF
