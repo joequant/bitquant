9b6400b5a372d4f4b3f5bbc92b4d1801f0d9af95
Joseph C Wang <joequant@gmail.com>
2018-10-22 21:39:06 +0800

    remove install

--- bitstation/docker-cauldron.sh
+++ bitstation/docker-cauldron.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
+dnf install -v -y --setopt=install_weak_deps=False --nodocs --allowerasing --best 'dnf-command(config-manager)' mageia-repos-cauldron 
 dnf shell -v -y  <<EOF
-install --setopt=install_weak_deps=False --nodocs --allowerasing --best 'dnf-command(config-manager)' mageia-repos-cauldron 
 repo disable mageia-x86_64
 repo disable updates-x86_64
 repo enable cauldron-x86_64
