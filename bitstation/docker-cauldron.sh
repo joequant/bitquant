9ed658e834b0b651f70b18c45cef36bc03b10e09
Joseph C Wang <joequant@gmail.com>
2018-10-22 21:07:59 +0800

    move to shell

--- bitstation/docker-cauldron.sh
+++ bitstation/docker-cauldron.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
-dnf install -v -y \
-     --setopt=install_weak_deps=False --nodocs --allowerasing --best \
-     'dnf-command(config-manager)' mageia-repos-cauldron
 dnf shell -v -y  <<EOF
+install --setopt=install_weak_deps=False --nodocs --allowerasing --best 'dnf-command(config-manager)' mageia-repos-cauldron 
 repo disable mageia-x86_64
 repo disable updates-x86_64
 repo enable cauldron-x86_64
 repo enable cauldron-x86_64-nonfree
 repo enable cauldron-x86_64-tainted
 config-manager  --add-repo http://mirrors.kernel.org/mageia/distrib/cauldron/x86_64/media/core/release cauldron
+upgrade --allowerasing --best --nodocs --setopt=install_weak_deps=False
 run
 EOF
-dnf upgrade -y -v --allowerasing --best --nodocs --setopt=install_weak_deps=False
+
