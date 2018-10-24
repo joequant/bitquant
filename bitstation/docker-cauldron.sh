c6374a1277bff0c893e238495d4c8fc29ddb0082
Joseph C Wang <joequant@gmail.com>
2018-10-24 22:36:15 +0800

    add dnf

--- bitstation/docker-cauldron.sh
+++ bitstation/docker-cauldron.sh
@@ -9,8 +9,8 @@ repo enable cauldron-x86_64
 repo enable cauldron-x86_64-nonfree
 repo enable cauldron-x86_64-tainted
 config-manager  --add-repo http://mirrors.kernel.org/mageia/distrib/cauldron/x86_64/media/core/release cauldron
-run
-upgrade --allowerasing --best --nodocs --setopt=install_weak_deps=False
-run
 EOF
+dnf upgrade -v -y --allowerasing --best --nodocs --setopt=install_weak_deps=False
+
+
 
