d61a06ffff1bb6fc1b3bb8846ad59d0689ac0224
Joseph C Wang <joequant@gmail.com>
2018-10-25 19:29:43 +0800

    change to use volume

--- bitstation/start-server.sh
+++ bitstation/start-server.sh
@@ -6,72 +6,17 @@ if [ "$1" != "" ] ; then
 fi
 
 if [ "$BTQNT_IMAGE_DIR" == "" ] ; then
-    BTQNT_IMAGE_DIR=~/volumes/bitstation
+    BTQNT_IMAGE_DIR=bitstation-vol
 fi
-
-$SUDO docker run $IMAGE echo 
-mkdir -p $BTQNT_IMAGE_DIR
-pushd $BTQNT_IMAGE_DIR
-mkdir -p var/lib
-chmod a+rwx var
-chmod a+rwx var/lib
-
-if [ ! -e $BTQNT_IMAGE_DIR/home ] ; then
-# This will fail on docker < 1.8
-id=$($SUDO docker create $IMAGE)
-$SUDO docker cp $id:/home - | tar xf -
-$SUDO docker rm -v $id
-fi
-
-if [ ! -e $BTQNT_IMAGE_DIR/var/log ] ; then
-mkdir -p var/log
-chmod a+rwx var/log
-$SUDO docker run \
--v $BTQNT_IMAGE_DIR/var:/mnt \
-$IMAGE \
-cp -a -P -R /var/log /mnt
-fi
-
-if [ ! -e $BTQNT_IMAGE_DIR/etc ] ; then
-mkdir -p etc
-chmod a+rwx etc
-$SUDO docker run \
--v $BTQNT_IMAGE_DIR:/mnt \
-$IMAGE \
-cp -a -P -R /etc /mnt
-fi
-
-root=var/lib
-for app in dokuwiki mongodb redis ; do
-if [ ! -e $BTQNT_IMAGE_DIR/$root/$app ] ; then
-$SUDO docker run \
--v $BTQNT_IMAGE_DIR/$root:/mnt \
-$IMAGE \
-cp -a -P -R /$root/$app /mnt
-fi
-done
-
-root=home/user/.local/share/jupyter/kernels
-for app in redis ; do
-if [ ! -e $BTQNT_IMAGE_DIR/$root/$app ] ; then
-$SUDO docker run \
--v $BTQNT_IMAGE_DIR/$root:/mnt \
-$IMAGE \
-cp -a -P -R /$root/$app /mnt
-fi
-done
-
-
-popd
 
 $SUDO docker run -i \
 --privileged --init \
--v $BTQNT_IMAGE_DIR/home:/home \
--v $BTQNT_IMAGE_DIR/var/lib/dokuwiki:/var/lib/dokuwiki \
--v $BTQNT_IMAGE_DIR/var/lib/mongodb:/var/lib/mongodb \
--v $BTQNT_IMAGE_DIR/var/lib/redis:/var/lib/redis \
--v $BTQNT_IMAGE_DIR/var/log:/var/log \
--v $BTQNT_IMAGE_DIR/etc:/etc \
+-v $BTQNT_IMAGE_DIR-home:/home \
+-v $BTQNT_IMAGE_DIR-dokuwiki:/var/lib/dokuwiki \
+-v $BTQNT_IMAGE_DIR-mongodb:/var/lib/mongodb \
+-v $BTQNT_IMAGE_DIR-redis:/var/lib/redis \
+-v $BTQNT_IMAGE_DIR-log:/var/log \
+-v $BTQNT_IMAGE_DIR-etc:/etc \
 -p 80:80 -p 443:443 $IMAGE >& docker.log &
 echo "Docker started"
 
