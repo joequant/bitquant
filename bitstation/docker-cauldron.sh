efa70c0370fbccc2208c24812f35398ff66603f6
Joseph C Wang <joequant@gmail.com>
2018-11-04 05:11:03 +0800

    put in kernel org

--- bitstation/docker-cauldron.sh
+++ bitstation/docker-cauldron.sh
@@ -9,6 +9,7 @@ repo disable updates-x86_64
 repo enable cauldron-x86_64
 repo enable cauldron-x86_64-nonfree
 repo enable cauldron-x86_64-tainted
+config-manager  --add-repo http://mirrors.kernel.org/mageia/distrib/cauldron/x86_64/media/core/release cauldron
 EOF
 dnf upgrade -v -y --allowerasing --best --nodocs --setopt=install_weak_deps=False -x filesystem
 
