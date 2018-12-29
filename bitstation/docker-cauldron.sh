c0fa6747bc3bf649f84f5091d5f61c703d98bda5
Joseph C Wang <joequant@gmail.com>
2018-12-29 23:30:58 +0800

    fix up dnf repo commands

--- bitstation/docker-cauldron.sh
+++ bitstation/docker-cauldron.sh
@@ -4,12 +4,9 @@ dnf install -v -y \
      --setopt=install_weak_deps=False --nodocs --allowerasing --best \
      'dnf-command(config-manager)' mageia-repos-cauldron
 dnf shell -v -y  <<EOF
-repo disable mageia-x86_64
-repo disable updates-x86_64
-repo enable cauldron-x86_64
-repo enable cauldron-x86_64-nonfree
-repo enable cauldron-x86_64-tainted
-config-manager  --add-repo http://mirrors.kernel.org/mageia/distrib/cauldron/x86_64/media/core/release cauldron
+config-manager --set-disabled mageia-x86_64 updates-x86_64
+config-manager --set-enabled cauldron-x86_64 cauldron-x86_64-nonfree cauldron-x86_64-tainted
+config-manager --add-repo http://mirrors.kernel.org/mageia/distrib/cauldron/x86_64/media/core/release cauldron
 EOF
 dnf upgrade -v -y --allowerasing --best --nodocs --setopt=install_weak_deps=False -x filesystem
 
