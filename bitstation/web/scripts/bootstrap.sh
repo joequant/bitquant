0c639dcc9497029fac2984ea7e29f29b2c840e1d
Joseph C Wang <joequant@gail.com>
2019-01-15 20:28:32 +0000

    fix directories

--- bitstation/web/scripts/bootstrap.sh
+++ bitstation/web/scripts/bootstrap.sh
@@ -2,7 +2,6 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 . $SCRIPT_DIR/environment.sh
 
-
 echo "Running from directory $GIT_DIR as user "`whoami`
 echo "Doing initial installation"
 echo "Installing misc"
@@ -12,11 +11,11 @@ $GIT_DIR/git/rebuild-misc.sh
 # install python first so that ijavascript dependencies
 # are met
 echo "Installing python packages"
-$GIT_DIR/web/scripts/install-python.sh
+$WEB_DIR/scripts/install-python.sh
 echo "Installing npm packages"
-$GIT_DIR/web/scripts/install-npm.sh
+$WEB_DIR/scripts/install-npm.sh
 echo "Installing R packages"
-$GIT_DIR/web/scripts/install-r-pkgs.sh
+$WEB_DIR/scripts/install-r-pkgs.sh
 
 #set wiki conf
 echo "Set up wiki"
@@ -25,11 +24,11 @@ sudo /usr/share/bitquant/conf.sh /wiki-init
 
 echo "Set up ipython"
 mkdir -p $MY_HOME/ipython
-ln -s -f ../git/bitquant/web/home/ipython/examples $MY_HOME/ipython/examples
+cp -r $WEB_DIR/home/ipython/examples $MY_HOME/ipython/examples
 
 echo "Set up R"
 mkdir -p $MY_HOME/R
-cp -r $GIT_DIR/web/home/R/* $MY_HOME/R
+cp -r $WEB_DIR/home/R/* $MY_HOME/R
 
 # Refresh configurations
 sudo /usr/share/bitquant/conf.sh /default-init
