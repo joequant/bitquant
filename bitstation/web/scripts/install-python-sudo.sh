e6690ec1156223fba010fdafbbd6cd6567d0bd4e
Joseph C Wang <joequant@gail.com>
2019-01-16 00:23:10 +0000

    add numpy

--- bitstation/web/scripts/install-python-sudo.sh
+++ bitstation/web/scripts/install-python-sudo.sh
@@ -26,6 +26,7 @@ popd
 pip3 install --upgrade pip --prefix /usr
 #remove to avoid attribute error
 pip3 uninstall numpy -y
+pip3 install --upgrade numpy --prefix /usr
 
 # reinstall to get jupyter executable
 pip3 install --upgrade --force-reinstall jupyter-core --prefix /usr
