cf939ab0bf04cb6444be38af75f54829c41c7d13
Joseph C Wang <joequant@gmail.com>
2019-02-04 23:44:50 +0800

    fixup cauldron build

--- bitstation/01.cauldron/docker-cauldron.sh
+++ bitstation/01.cauldron/docker-cauldron.sh
@@ -8,6 +8,7 @@ config-manager --set-disabled mageia-x86_64 updates-x86_64
 config-manager --set-enabled cauldron-x86_64 cauldron-x86_64-nonfree cauldron-x86_64-tainted
 EOF
 dnf config-manager --add-repo http://mirrors.kernel.org/mageia/distrib/cauldron/x86_64/media/core/release cauldron
+dnf upgrade -v -y --allowerasing --best --nodocs --setopt=install_weak_deps=False chkconfig
 dnf upgrade -v -y --allowerasing --best --nodocs --setopt=install_weak_deps=False -x filesystem
 
 
