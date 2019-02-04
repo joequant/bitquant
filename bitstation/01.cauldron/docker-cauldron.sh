1d0cf22572684a3c10336a3e10652fc22c184160
Joseph C Wang <joequant@gmail.com>
2019-02-04 23:05:21 +0800

    move repos

--- bitstation/01.cauldron/docker-cauldron.sh
+++ bitstation/01.cauldron/docker-cauldron.sh
@@ -7,6 +7,7 @@ dnf shell -v -y  <<EOF
 config-manager --set-disabled mageia-x86_64 updates-x86_64
 config-manager --set-enabled cauldron-x86_64 cauldron-x86_64-nonfree cauldron-x86_64-tainted
 EOF
+dnf config-manager --add-repo http://mirrors.kernel.org/mageia/distrib/cauldron/x86_64/media/core/release cauldron
 dnf upgrade -v -y --allowerasing --best --nodocs --setopt=install_weak_deps=False -x filesystem
 
 
