41163159a78408d98d50faa3d34b8235389f0dc9
Joseph C Wang <joequant@gmail.com>
2019-02-07 01:27:00 +0800

    fix typo

--- bitstation/03.setup/install-ruby.sh
+++ bitstation/03.setup/install-ruby.sh
@@ -3,7 +3,7 @@ set -v
 gem install --no-ri --no-rdoc cztop ffi-rzmq
 
 #disable Werror
-CPPFLAGS="-Wno-error=deprecated-declarations" gem install --no-ro --no-rdoc rbczmq
+CPPFLAGS="-Wno-error=deprecated-declarations" gem install --no-ri --no-rdoc rbczmq
 gem install --no-ri --no-rdoc iruby --pre
 iruby register --force
 
