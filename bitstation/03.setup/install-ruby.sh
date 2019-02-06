41646aa8041cb8cf970322a501139cbb2b3eeb94
Joseph C Wang <joequant@gmail.com>
2019-02-06 21:19:57 +0800

    fix up iruby build

--- bitstation/03.setup/install-ruby.sh
+++ bitstation/03.setup/install-ruby.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -v
-gem install cztop rbczmq ffi_rzmq
+gem install cztop ffi-rzmq
+
+#disable Werror
+CPPFLAGS="-Wno-error=deprecated-declarations" gem install rbczmq
 gem install iruby --pre
 iruby register --force
 
