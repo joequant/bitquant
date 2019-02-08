f20af24227331f726955464c3c96171319a93f99
Joseph C Wang <joequant@gmail.com>
2019-02-08 21:44:52 +0800

    fix ruby

--- bitstation/03.setup/install-ruby.sh
+++ bitstation/03.setup/install-ruby.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # TODO - fixes
 set -v
-gem install --no-ri --no-rdoc cztop ffi-rzmq
+gem install --no-ri --no-rdoc cztop ffi-rzmq -n /usr/bin -i /usr/share/gems
 
 #disable Werror
-CPPFLAGS="-Wno-error=deprecated-declarations" gem install --no-ri --no-rdoc rbczmq
-gem install --no-ri --no-rdoc iruby --pre
+CPPFLAGS="-Wno-error=deprecated-declarations" gem install --no-ri --no-rdoc rbczmq -n /usr/bin -i /usr/share/gems
+gem install --no-ri --no-rdoc iruby -n /usr/bin -i /usr/share/gems
 iruby register --force
 mv /root/.ipython/kernels/ruby /usr/share/jupyter/kernels
-mv /usr/local/bin/* /usr/bin
+
