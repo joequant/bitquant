93c0739878cc840ce1a83b3af2a063f7a0d47a7c
Joseph C Wang <joequant@gmail.com>
2019-03-01 01:21:11 +0800

    add start server

--- bitstation/start-server.sh
+++ bitstation/start-server.sh
@@ -9,8 +9,7 @@ if [ "$BTQNT_IMAGE_DIR" == "" ] ; then
     BTQNT_IMAGE_DIR=bitstation-vol
 fi
 
-$SUDO docker run -i \
-      --privileged --init \
+$SUDO docker run -t -i \
       -v $BTQNT_IMAGE_DIR-home:/home \
       -v $BTQNT_IMAGE_DIR-dokuwiki:/var/lib/dokuwiki \
       -v $BTQNT_IMAGE_DIR-mongodb:/var/lib/mongodb \
@@ -18,6 +17,7 @@ $SUDO docker run -i \
       -v $BTQNT_IMAGE_DIR-bitcoin:/var/lib/bitcoin \
       -v $BTQNT_IMAGE_DIR-log:/var/log \
       -v $BTQNT_IMAGE_DIR-etc:/etc \
+      -v $BTQNT_IMAGE_DIR-srv:/srv \
       -p 80:80 -p 443:443 $IMAGE >& docker.log &
 echo "Docker started"
 
