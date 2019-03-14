6948a53ae7eff447c6e5d4f8fe8678aed4ae96f0
Joseph C Wang <joequant@gmail.com>
2019-03-15 01:22:13 +0800

    add reticulate

--- bitstation/web/scripts/install-r-pkgs.sh
+++ bitstation/web/scripts/install-r-pkgs.sh
@@ -17,7 +17,7 @@ export PATH=/usr/lib64/R/bin:/usr/lib/R/bin:$PATH
 mkdir -p $LOCAL_R_DIR
 mkdir -p /home/$ME/attic/$$
 echo "Generating new modules"
-/usr/bin/R -e "install.packages(c('stringi', 'magrittr', 'devtools', 'crayon', 'pbdZMQ'), repos='http://cran.r-project.org/', dependencies=TRUE)"
+/usr/bin/R -e "install.packages(c('stringi', 'magrittr', 'devtools', 'crayon', 'pbdZMQ', 'reticulate'), repos='http://cran.r-project.org/', dependencies=TRUE)"
 /usr/bin/R -e "install.packages(c('shiny','Quandl','knitr', 'rzmq'), repos='http://cran.r-project.org/')"
 /usr/bin/R -e 'options(repos=c(CRAN = "http://cran.r-project.org/")); library(devtools) ; devtools::install_github("rstudio/rmarkdown")'
 /usr/bin/R -e 'options(repos=c(CRAN = "http://cran.r-project.org/")); library(devtools) ; install_github("IRkernel/repr"); devtools::install_github("IRKernel/IRdisplay"); devtools::install_github("IRKernel/IRkernel")'
