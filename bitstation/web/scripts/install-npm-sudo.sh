6b40353efc6596cf2d94dd2406e5e58d6106c976
Joseph C Wang <joequant@gmail.com>
2019-03-18 08:48:02 +0800

    move npm to setup

--- bitstation/web/scripts/install-npm-sudo.sh
+++ bitstation/web/scripts/install-npm-sudo.sh
@@ -8,16 +8,6 @@ ROOT_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 SCRIPT_DIR=$1
 ME=$2
 
-npm install -g node-gyp coffeescript livescript
-node-gyp -g install
-# put yelp_uri in back to override downloaded version
-npm install -g --unsafe ijavascript configurable-http-proxy solc jp-coffeescript jp-livescript modclean
-ijsinstall --install=global
-jp-coffee-install --install=global
-jp-livescript-install --install=global
-mkdir -p /usr/share/jupyter/kernels
-mv /usr/local/share/jupyter/kernels/* /usr/share/jupyter/kernels
-
 pushd /home/$ME/git/ethercalc
 npm i -g --unsafe ethercalc
 popd
