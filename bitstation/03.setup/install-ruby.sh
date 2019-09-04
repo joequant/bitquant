de5cf8799ef6a0eb04347f988fec40fb919c36b2
Joseph C Wang <joequant@gmail.com>
2019-09-05 01:09:53 +0800

    fix iruby

--- bitstation/03.setup/install-ruby.sh
+++ bitstation/03.setup/install-ruby.sh
@@ -2,12 +2,14 @@
 # TODO - fixes
 set -v
 export CPPFLAGS="-Wno-error=deprecated-declarations"
+export JUPYTER_DATA_DIR=/usr/share/jupyter
+
 gem install --no-ri --no-rdoc ffi-rzmq -n /usr/bin -i /usr/share/gems
 
 #disable Werror
-gem install --no-ri --no-rdoc iruby -n /usr/bin -i /usr/share/gems
+gem install --no-ri --no-rdoc iruby -n /usr/bin -i /usr/share/gems --pre
 gem install --no-ri --no-rdoc pry awesome_print gnuplot rubyvis nyaplot pry-doc -n /usr/bin -i /usr/share/gems
 iruby register --force
-mv /root/.ipython/kernels/ruby /usr/share/jupyter/kernels
+#mv /root/.ipython/kernels/ruby /usr/share/jupyter/kernels
 
 # remember to link libzmq.so.5 to libzmq.so
