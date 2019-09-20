7de149e0a7abdc55182d6bc47faea9f9c4f34568
Joseph C Wang <joequant@gmail.com>
2019-09-21 05:03:13 +0800

    fix up dockerfile

--- bitstation/01.cauldron/docker-cauldron.sh
+++ bitstation/01.cauldron/docker-cauldron.sh
@@ -5,6 +5,7 @@ set -e
 cat <<EOF >> /etc/dnf/dnf.conf
 fastestmirror=true
 excludepkgs=filesystem,chkconfig
+max_parallel_downloads=10
 EOF
 
 dnf install -v -y \
@@ -18,7 +19,6 @@ EOF
 dnf config-manager --add-repo http://mirrors.kernel.org/mageia/distrib/cauldron/x86_64/media/core/release cauldron
 dnf upgrade -v -y --allowerasing --best --nodocs --setopt=install_weak_deps=False
 rpm -qa | grep mga6 | xargs dnf autoremove -y -x filesystem -x chkconfig
-rpm --erase info-install
 dnf autoremove -y urpmi
 dnf clean all
 rm -f /var/log/*.log
