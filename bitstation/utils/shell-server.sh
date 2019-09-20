156ab5f91d14adc8c39e71882e9a57e75ddba6ca
Joseph C Wang <joequant@gmail.com>
2019-09-21 04:59:16 +0800

    add features to shell

--- bitstation/utils/shell-server.sh
+++ bitstation/utils/shell-server.sh
@@ -2,11 +2,16 @@
 export LANG=C LC_ALL=C
 
 if [ $# -ge 1 ]; then
-IMAGE=$1
+    IMAGE=$($SUDO docker ps | tail -n +2 | grep $1 | awk '{print $NF}' )
+    echo $IMAGE
 else 
-IMAGE=$($SUDO docker ps | awk 'FNR==2 {print $NF}')
-#IMAGE=bitstation
+    IMAGE=$($SUDO docker ps | awk 'FNR==2 {print $NF}')
 fi
 
-exec $SUDO docker exec -it $IMAGE /bin/bash
+if [ $# -ge 2 ]; then
+    CMD=$2
+else
+    CMD=/bin/bash
+fi
 
+exec $SUDO docker exec -it $IMAGE $CMD
