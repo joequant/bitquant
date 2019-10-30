161a3c039c72a96fffc76b1c66b65d560fd9eb07
Joseph C Wang <joequant@gmail.com>
2019-10-31 03:36:03 +0800

    bootstrap with urpmi

--- bitstation/01.cauldron/docker-cauldron.sh
+++ bitstation/01.cauldron/docker-cauldron.sh
@@ -1,26 +1,31 @@
 #!/bin/bash
-set -e
+set -e -v
 . /tmp/proxy.sh
 
 cat <<EOF >> /etc/dnf/dnf.conf
 fastestmirror=true
 excludepkgs=filesystem,chkconfig
-max_parallel_downloads=10
+max_parallel_downloads=15
 EOF
 
-dnf install -v -y \
-     --setopt=install_weak_deps=False --nodocs --allowerasing --best \
-     'dnf-command(config-manager)' mageia-repos-cauldron --nogpgcheck
+#dnf install -v -y \
+#     --setopt=install_weak_deps=False --nodocs --allowerasing --best \
+#     'dnf-command(config-manager)' mageia-repos-cauldron --nogpgcheck
+
+urpmi.update -a
+urpmi -y mageia-repos-cauldron 'dnf-command(config-manager)'
 dnf shell -v -y  <<EOF
 config-manager --set-disabled mageia-x86_64 updates-x86_64
 config-manager --set-enabled cauldron-x86_64 cauldron-x86_64-nonfree cauldron-x86_64-tainted
 EOF
 
 dnf config-manager --add-repo http://mirrors.kernel.org/mageia/distrib/cauldron/x86_64/media/core/release cauldron
+rm /etc/yum.repos.d/mageia-*
 dnf upgrade -v -y --allowerasing --best --nodocs --setopt=install_weak_deps=False
-rpm -qa | grep mga6 | xargs dnf autoremove -y -x filesystem -x chkconfig
+
 dnf autoremove -y urpmi
 dnf clean all
+
 rm -f /var/log/*.log
 rm -rf /var/cache/dnf/*
 rm -rf /usr/lib/udev
