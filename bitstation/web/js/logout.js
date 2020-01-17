74c4a61f409ea61ac4266066c8fbf193a5f6f7a8
Joseph C Wang <joequant@gmail.com>
2020-01-18 03:41:45 +0800

    fix up logout

--- bitstation/web/js/logout.js
+++ bitstation/web/js/logout.js
@@ -1,8 +1,9 @@
 function logout() {
     var out = window.location.href.replace(/:\/\//, '://log:out@');
 
-    jQuery.get(out).error(function() {
+    jQuery.get(out).always(function() {
+        jQuery.get('/jupyterhub/hub/logout').always(function () {
         window.location = '/dokuwiki';
+});
     });
     return false;
-}
