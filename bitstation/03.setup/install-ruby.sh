4fbede54944137c9ff306e34f751a3f6596c72b7
Joseph C Wang <joequant@gmail.com>
2020-03-04 21:05:15 +0800

    fix ruby build for 2.7.0

--- bitstation/03.setup/install-ruby.sh
+++ bitstation/03.setup/install-ruby.sh
@@ -4,11 +4,12 @@ set -v
 export CPPFLAGS="-Wno-error=deprecated-declarations"
 export JUPYTER_DATA_DIR=/usr/share/jupyter
 
-gem install --no-ri --no-rdoc ffi-rzmq -n /usr/bin -i /usr/share/gems
+gem install --no-document ffi -n /usr/bin -i /usr/share/gems -- --with-ldflags="-pthread -ldl"
+gem install --no-document ffi-rzmq -n /usr/bin -i /usr/share/gems
 
 #disable Werror
-gem install --no-ri --no-rdoc iruby -n /usr/bin -i /usr/share/gems --pre
-gem install --no-ri --no-rdoc pry awesome_print gnuplot rubyvis nyaplot pry-doc -n /usr/bin -i /usr/share/gems
+gem install --no-document iruby -n /usr/bin -i /usr/share/gems --pre
+gem install --no-document pry awesome_print gnuplot rubyvis nyaplot pry-doc -n /usr/bin -i /usr/share/gems
 iruby register --force
 #mv /root/.ipython/kernels/ruby /usr/share/jupyter/kernels
 
