a5b4f521abbd4bff97926fc2d7e7d30938e76c52
Joseph C Wang <joequant@gmail.com>
2020-03-07 01:12:52 +0800

    fix logout to cleanly log out of dokuwiki

--- bitstation/web/js/logout.js
+++ bitstation/web/js/logout.js
@@ -1,9 +1,9 @@
 function logout() {
     var out = window.location.href.replace(/:\/\//, '://log:out@');
-
     jQuery.get(out).always(function() {
-        jQuery.get('/jupyterhub/hub/logout').always(function () {
-        window.location = '/dokuwiki';
+        jQuery.get('/jupyterhub/hub/logout').always(function () {});
+    jQuery.get('/dokuwiki/doku.php?do=logout').always(function() {
+    window.location.assign('/dokuwiki'); reload(true);
 });
     });
     return false;
