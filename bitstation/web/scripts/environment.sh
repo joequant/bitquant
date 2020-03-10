1ec3a39896ac08c6df35e4f1e5e0b7eb3debd857
Joseph C Wang <joequant@gmail.com>
2020-03-10 22:31:30 +0800

    change code to user

--- bitstation/web/scripts/environment.sh
+++ bitstation/web/scripts/environment.sh
@@ -1,11 +1,5 @@
-# use ls because stat causes error on dockerhub
-ME=$(ls -ld ${BASH_SOURCE[0]} | awk '{print $3}')
-GROUP=$(ls -ld ${BASH_SOURCE[0]} | awk '{print $4}')
-
-if [[ $ME == "root" ]] ; then
-echo "Owner of environment.sh should not be root"
-exit 1
-fi
+ME=user
+GROUP=user
 MY_HOME=$(eval echo ~$ME)
 # needed for building ethercalc
 USERPROFILE=$MY_HOME
