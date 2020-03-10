b4458d864e661c0d7dc008d68481d67dbbf51197
Joseph C Wang <joequant@gmail.com>
2020-03-10 16:15:26 +0800

    add rmisc and ggplot2

--- bitstation/02.setup/install-r-pkgs.sh
+++ bitstation/02.setup/install-r-pkgs.sh
@@ -15,7 +15,7 @@ mkdir -p $LOCAL_R_DIR
 mkdir -p /home/user/attic/$$
 echo "Generating new modules"
 /usr/bin/R -e "install.packages(c('stringi', 'magrittr', 'devtools', 'crayon', 'pbdZMQ', 'reticulate'), repos='http://cran.r-project.org/', dependencies=TRUE)"
-/usr/bin/R -e "install.packages(c('shiny','Quandl','knitr', 'rzmq'), repos='http://cran.r-project.org/')"
+/usr/bin/R -e "install.packages(c('shiny','Quandl','knitr', 'rzmq'. 'ggplot2', 'Rmisc'), repos='http://cran.r-project.org/')"
 /usr/bin/R -e 'options(repos=c(CRAN = "http://cran.r-project.org/")); library(devtools) ; devtools::install_github("rstudio/rmarkdown")'
 /usr/bin/R -e 'options(repos=c(CRAN = "http://cran.r-project.org/")); library(devtools) ; install_github("IRkernel/repr"); devtools::install_github("IRKernel/IRdisplay"); devtools::install_github("IRKernel/IRkernel")'
 
