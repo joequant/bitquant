ba512cdbac067c987c2e59fb1e6e09e58c98429f
Joseph C Wang <joequant@gmail.com>
2020-03-13 02:42:17 +0800

    set unsafe-perm

--- bitstation/02.setup/install-npm.sh
+++ bitstation/02.setup/install-npm.sh
@@ -3,17 +3,12 @@
 
 pushd /home/user
 
-#see https://github.com/npm/npm/issues/3565#issuecomment-202473011
-# work around npm not working EPERM errors
-
-echo "int chown() { return 0; }" > preload.c && gcc -shared -o preload.so preload.c 
-
 npm install -g node-gyp coffeescript
 
 # livescript
 node-gyp -g install
 # put yelp_uri in back to override downloaded version
-LD_PRELOAD=/home/user/preload.so npm install -g --unsafe ijavascript configurable-http-proxy solc jp-coffeescript itypescript jp-babel modclean
+npm install -g --unsafe-perm=true ijavascript configurable-http-proxy solc jp-coffeescript itypescript jp-babel modclean
 
 #jp-livescript
 
@@ -32,7 +27,7 @@ mv /usr/local/share/jupyter/kernels/* /usr/share/jupyter/kernels
 
 if [ -d /home/user/git/ethercalc ] ; then
     pushd /home/user/git/ethercalc
-    LD_PRELOAD=/home/user/preload.so npm i -g --unsafe ethercalc
+    npm i -g --unsafe-perm=true ethercalc
     popd
 fi
 popd
