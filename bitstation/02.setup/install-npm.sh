555d113a1ac67611896b48958141126ced859ccb
Joseph C Wang <joequant@gmail.com>
2020-03-12 17:40:35 +0800

    fix eperm

--- bitstation/02.setup/install-npm.sh
+++ bitstation/02.setup/install-npm.sh
@@ -13,7 +13,7 @@ npm install -g node-gyp coffeescript
 # livescript
 node-gyp -g install
 # put yelp_uri in back to override downloaded version
-npm install -g --unsafe ijavascript configurable-http-proxy solc jp-coffeescript itypescript jp-babel modclean
+LD_PRELOAD=/home/user/preload.so npm install -g --unsafe ijavascript configurable-http-proxy solc jp-coffeescript itypescript jp-babel modclean
 
 #jp-livescript
 
