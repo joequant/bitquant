3b9cf23db64ac68fa8c820b49a7f76bc486a8764
Joseph C Wang <joequant@gmail.com>
2020-03-22 22:50:17 +0800

    extend timeouts and memory limits

--- nextcloud/install.sh
+++ nextcloud/install.sh
@@ -22,9 +22,16 @@ cp /tmp/config.php /etc/nextcloud
 cp /tmp/nextcloud.inc /etc/httpd/conf/webapps.d
 touch /etc/nextcloud/CAN_INSTALL
 
+cat <<EOF > /etc/php-fpm.d/mem-limit.conf
+[www]
+php_admin_value[memory_limit] = 512M
+EOF
+
 pushd /etc/httpd/conf
 cp /tmp/00_mpm.conf modules.d
 if [ -e modules.d/00-php-fpm.conf ] ; then
     mv modules.d/00-php-fpm.conf modules.d/10-php-fpm.conf
 fi
 popd
+
+sed -i -e 's:120;:1200;:'  /usr/share/nextcloud/lib/private/Installer.php
