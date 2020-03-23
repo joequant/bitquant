e54ae60fc52d52bb17a3dee2d684f3c14937047f
Joseph C Wang <joequant@gmail.com>
2020-03-24 00:06:53 +0800

    add more fixes

--- nextcloud/install.sh
+++ nextcloud/install.sh
@@ -19,27 +19,8 @@ dnf --setopt=install_weak_deps=False --best install -v -y \
     php-pcntl \
     openssl
 
-
 cp /tmp/startup.sh /root
 cp /tmp/config.php /etc/nextcloud
-cp /tmp/nextcloud.inc /etc/httpd/conf/webapps.d
-cp /tmp/nextcloud.conf /etc/httpd/conf/webapps.d
 touch /etc/nextcloud/CAN_INSTALL
-
-cat <<EOF > /etc/php-fpm.d/nextcloud.conf
-[www]
-php_admin_value[upload_max_filesize] = 10G
-php_admin_value[post_max_size] = 10G
-php_admin_value[memory_limit] = 512M
-php_admin_value[max_input_time] = 86400
-php_admin_value[max_execution_time] = 86400
-EOF
-
-pushd /etc/httpd/conf
-cp /tmp/00_mpm.conf modules.d
-if [ -e modules.d/00-php-fpm.conf ] ; then
-    mv modules.d/00-php-fpm.conf modules.d/10-php-fpm.conf
-fi
-popd
 
 sed -i -e 's:120;:1200;:'  /usr/share/nextcloud/lib/private/Installer.php
--- nextcloud/startup.sh
+++ nextcloud/startup.sh
@@ -1,6 +1,51 @@
 #!/bin/bash
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+if [ -e /etc/nextcloud/CAN_INSTALL ] ; then
+    echo "waiting..."
+    sleep 10
+chown apache:apache -R /usr/share/nextcloud
+pushd /usr/share/nextcloud
+sudo -u apache php -d memory_limit=512M \
+     occ maintenance:install \
+     --database="pgsql" --database-name="nextcloud" \
+     --database-host="db" --database-user="postgres" \
+     --database-pass="mypass" --admin-user="admin" \
+     --admin-pass="password" --data-dir="/var/lib/nextcloud/data"
+chown root:root -R /usr/share/nextcloud
+
+sudo -u apache php occ config:system:set \
+     trusted_domains 1 "--value=*"
+sleep 10
+for app in \
+	onlyoffice \
+	calendar \
+        maps \
+	groupfolders \
+	sociallogin \
+	documentserver_community
+do
+sudo -u apache php -d memory_limit=512M \
+     occ app:enable $app
+done
+popd
+rm /etc/nextcloud/CAN_INSTALL
+fi
+
+: ' \
+	calendar \
+	maps \
+	contacts \
+	mail \
+	tasks \
+
+	files_markdown \
+	notes \
+	files_mindmap \
+	deck \
+'
+
+
 if [ -x /usr/sbin/php-fpm ] ; then
 echo "Restarting php-fpm"
 /usr/sbin/php-fpm --nodaemonize --fpm-config /etc/php-fpm.conf >> /var/log/php-fpm.log 2>&1 &
