a3ae8d6a2ebb7a7ed43022568ee9a742ac26eada
Joseph C Wang <joequant@gmail.com>
2020-03-24 12:49:19 +0800

    remove share

--- nextcloud/backup.sh
+++ nextcloud/backup.sh
@@ -2,5 +2,4 @@
 DATE=$(date -u +'%Y%m%d.%H%M%S')
 IMAGE=$($SUDO docker ps | tail -n +2 | grep nextcloud_db | awk '{print $NF}' )
 docker exec -it $IMAGE pg_dumpall -U postgres | xz - > nextcloud_db.$DATE.dmp.xz
-docker run -v nextcloud_share:/volume --rm loomchild/volume-backup backup -c xz - > nextcloud_share.$DATE.tar.xz
 docker run -v nextcloud_lib:/volume --rm loomchild/volume-backup backup -c xz - > nextcloud_lib.$DATE.tar.xz
