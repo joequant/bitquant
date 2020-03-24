365cc01aac94f3df0093c48c7c9e11cd35d09e2e
Joseph C Wang <joequant@gmail.com>
2020-03-25 04:59:18 +0800

    dump database as volume

--- nextcloud/backup.sh
+++ nextcloud/backup.sh
@@ -1,5 +1,6 @@
 #/bin/bash -f
 DATE=$(date -u +'%Y%m%d.%H%M%S')
 IMAGE=$($SUDO docker ps | tail -n +2 | grep nextcloud_db | awk '{print $NF}' )
-docker exec -it $IMAGE pg_dumpall -U postgres | xz - > nextcloud_db.$DATE.dmp.xz
+
 docker run -v nextcloud_lib:/volume --rm loomchild/volume-backup backup -c xz - > nextcloud_lib.$DATE.tar.xz
+docker run -v nextcloud_db:/volume --rm loomchild/volume-backup backup -c xz - > nextcloud_db.$DATE.tar.xz
