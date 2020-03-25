85cdc8fba14776e715a6a812f25ae8cf55e8c34f
Joseph C Wang <joequant@gmail.com>
2020-03-25 12:17:54 +0800

    add etc

--- nextcloud/backup.sh
+++ nextcloud/backup.sh
@@ -4,3 +4,4 @@ IMAGE=$($SUDO docker ps | tail -n +2 | grep nextcloud_db | awk '{print $NF}' )
 
 docker run -v nextcloud_lib:/volume --rm loomchild/volume-backup backup -c xz - > nextcloud_lib.$DATE.tar.xz
 docker run -v nextcloud_db:/volume --rm loomchild/volume-backup backup -c xz - > nextcloud_db.$DATE.tar.xz
+docker run -v nextcloud_etc:/volume --rm loomchild/volume-backup backup -c xz - > nextcloud_etc.$DATE.tar.xz
