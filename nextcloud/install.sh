d84f1bff5edbef0e502b51955f48b42146c6aa0c
Joseph C Wang <joequant@gmail.com>
2020-03-30 07:27:23 +0800

    remove locale

--- nextcloud/install.sh
+++ nextcloud/install.sh
@@ -31,3 +31,7 @@ sed -i -e 's:120;:1200;:'  /usr/share/nextcloud/lib/private/Installer.php
 rm -rf /usr/lib/.build-id
 rm -rf /usr/lib/python3.7
 rm -rf /usr/lib/systemd
+
+pushd /usr/share/locale
+rm -rf `ls | grep -v "^ISO" | grep -v "^UTF" | grep -v "^en" | grep -v "^C.UTF"`
+popd
