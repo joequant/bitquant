7768c7564ca810f2248432d72bf15e177a4940ed
Joseph C Wang <joequant@gmail.com>
2020-04-10 21:25:09 +0800

    add logout

--- bitstation/web/js/logout.js
+++ bitstation/web/js/logout.js
@@ -2,7 +2,7 @@ function logout() {
     var out = window.location.href.replace(/:\/\//, '://log:out@');
     jQuery.get(out).always(function() {
 	jQuery.get('/jupyterhub/hub/logout').always(function () {});
-	jQuery.get('/nextcloud/logout').always(function () {});
+	jQuery.get('/nextcloud/index.php/logout').always(function () {});
     jQuery.get('/dokuwiki/doku.php?do=logout').always(function() {
     window.location.assign('/'); reload(true);
 });
