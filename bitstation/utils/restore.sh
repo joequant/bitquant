603fe678dda9f19e3e51f7cde5a422c60104778a
Joseph C Wang <joequant@gmail.com>
2020-05-20 01:32:57 +0800

    add features to restore

--- bitstation/utils/restore.sh
+++ bitstation/utils/restore.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
+CMD=docker
+
+while getopts 'c:' OPTION; do
+  case "$OPTION" in
+    c)
+      CMD="$OPTARG"
+      ;;
+    ?)
+      echo "script usage: -c cmd image" >&2
+      exit 1
+      ;;
+  esac
+done
+shift "$(($OPTIND -1))"
+ID=${1}
+
 IFS='.'
-read -ra ARR <<< $(basename "$1")
+read -ra ARR <<< $(basename "$ID")
 IFS=' '
 
 volname="${ARR[0]}"
@@ -8,6 +24,6 @@ compression="${ARR[-1]}"
 
 echo "Moving ${1} into ${volname}"
 
-cat  $1 |  podman run -i -v $volname:/volume --rm loomchild/volume-backup restore -c $compression -
+cat  $1 |  $CMD run -i -v $volname:/volume --rm loomchild/volume-backup restore -c $compression -
 #cat demo_home.20200503.113330.tar.xz  | tar -J -tf -
 
