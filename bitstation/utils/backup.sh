385626bd7e9df07fad1349bed6533746bb76e668
Joseph C Wang <joequant@gmail.com>
2020-06-01 19:58:16 +0800

    add backup script

---
+++ bitstation/utils/backup.sh
@@ -0,0 +1,33 @@
+#/bin/bash -f
+DATE=$(date -u +'%Y%m%d.%H%M%S')
+CMD=docker
+dir=`pwd`
+while getopts 'c:d:' OPTION; do
+  case "$OPTION" in
+    c)
+      CMD="$OPTARG"
+      ;;
+    d)
+	dir="$OPTARG"
+	if [ ! -d $dir ] ; then
+	    echo "cannot find directory $dir"
+	    exit 1
+	fi
+	;;
+    ?)
+      echo "script usage: -c cmd image" >&2
+      exit 1
+      ;;
+  esac
+done
+shift "$(($OPTIND -1))"
+ID=${1:-bitstation}
+
+pushd $dir
+VOLUMES=$($CMD volume ls | awk 'FNR >=2 {print $NF}' | grep ^$ID)
+
+for vol in $VOLUMES; do
+    echo $vol
+    $CMD run -v $vol:/volume --rm loomchild/volume-backup backup -c xz - > ${vol}.$DATE.tar.xz
+done
+popd
