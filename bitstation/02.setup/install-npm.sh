4a31b6cdbc0ddec5fb7e8cd5679e14db9233bb81
Joseph C Wang <joequant@gmail.com>
2020-06-29 01:19:59 +0800

    add livescript / typescript

--- bitstation/02.setup/install-npm.sh
+++ bitstation/02.setup/install-npm.sh
@@ -11,10 +11,11 @@ node-gyp -g install
 # put yelp_uri in back to override downloaded version
 npm install -g --unsafe-perm=true ijavascript --zmq-external
 
-# disable because of
+# use git because of
 # https://github.com/nearbydelta/itypescript/issues/20
-#npm install -g --unsafe-perm=true itypescript --zmq-external
+npm install -g --unsafe-perm=true git+https://github.com/joequant/itypescript.git --zmq-external
 npm install -g --unsafe-perm=true jp-coffeescript --zmq-external
+npm install -g --unsafe-perm=true git+https://github.com/joequant/jp-livescript.git --zmq-external
 npm install -g --unsafe-perm=true jp-babel --zmq-external
 npm install -g --unsafe-perm=true configurable-http-proxy solc modclean
 
@@ -23,12 +24,14 @@ npm install -g --unsafe-perm=true configurable-http-proxy solc modclean
 # jupyter --version is breaking installs
 # https://github.com/n-riesco/ijavascript/issues/200
 # fixed for all but livescript
-# jp-livescript-install --install=global
+# 
+
 
 ijsinstall --install=global
-#its --install=global
+its --install=global
 jp-coffee-install --install=global
 jp-babel-install --install=global
+jp-livescript-install --install=global
 
 mkdir -p /usr/share/jupyter/kernels
 mv /usr/local/share/jupyter/kernels/* /usr/share/jupyter/kernels
