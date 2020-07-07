3504c5d9e204dbfbdf2686666c431e3443d3e1c0
Joseph C Wang <joequant@gmail.com>
2020-07-08 06:25:08 +0800

    fix containters script

--- bitstation/utils/containers-down.sh
+++ bitstation/utils/containers-down.sh
@@ -3,8 +3,18 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 CONFIG_DIR=$SCRIPT_DIR/../config
 
 cd $CONFIG_DIR
-for f in *; do
+if [ -z $1  ] ; then
+    images=`echo */`
+else
+    images=$1
+fi
+for f in $images; do
     if [ -d "$f" ]; then
-	docker-compose -f $f/docker-compose.yml down
+	docker-compose -f $f/docker-compose.yml down &
     fi
+done
+for job in `jobs -p`
+do
+echo $job
+    wait $job || let "FAIL+=1"
 done
--- bitstation/utils/containers-up.sh
+++ bitstation/utils/containers-up.sh
@@ -5,11 +5,13 @@ LOG_DIR=$SCRIPT_DIR/../log
 DATE=$(date -u +%Y%m%d%H%M%S)
 
 cd $CONFIG_DIR
-for f in *; do
+if [ -z $1  ] ; then
+    images=`echo */`
+else
+    images=$1
+fi
+for f in $images; do
     if [ -d "$f" ]; then
-	docker-compose -f $f/docker-compose.yml up > $LOG_DIR/$f.$DATE.log &
+	docker-compose -f $f/docker-compose.yml up > $LOG_DIR/`basename $f`.$DATE.log &
     fi
 done
-
-
-
