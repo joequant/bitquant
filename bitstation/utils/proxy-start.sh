6f7ac033da0abea808dd1e1e6903b38f0a664a6e
Joseph C Wang <joequant@gmail.com>
2020-07-11 23:58:58 +0800

    fix up batch

--- bitstation/utils/proxy-start.sh
+++ bitstation/utils/proxy-start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 mkdir -p log
 devpi-server >> log/devpi-server.log 2>&1 &
-git-cache-http-server >> log/git-cache-http-server.log 2>&1 &
+git-cache-http-server $GIT_CACHE_ARGS >> log/git-cache-http-server.log 2>&1 &
 verdaccio >> log/verdaccio.log 2>&1 &
