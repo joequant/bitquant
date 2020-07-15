89a133baf44ef2f7c835b9c03d3ef8f1e6a60aa1
Joseph C Wang <joequant@gmail.com>
2020-07-15 09:13:27 +0800

    add readme and proxy.sh

---
+++ cacher/README.md
@@ -0,0 +1,8 @@
+Cacher is a image that contains various caching items
+
+squid - caches web downloads - also picks the fastest mageia RPMS
+distcc/ccache - caches compiles
+git-cache-httpd-server - caches git downloads
+verdaccio - caches npm
+
+To run the cache source proxy.sh with the location of the cache server
--- cacher/proxy.sh
+++ cacher/proxy.sh
@@ -1,11 +1,13 @@
-#: '
-timeout 1 bash -c 'cat < /dev/null > /dev/tcp/172.17.0.1/3128'
+# this is the location of the main server from a docker image
+cache_server=172.17.0.1
+
+timeout 1 bash -c 'cat < /dev/null > /dev/tcp/$cache_server/3128'
 if [ $? == 0 ] ; then
     echo "running proxy"
-export http_proxy=http://172.17.0.1:3128/
-export https_proxy=http://172.17.0.1:3128/
-export ftp_proxy=http://172.17.0.1:3128/
-export HTTP_PROXY=http://172.17.0.1:3128/
+export http_proxy=http://$cache_server:3128/
+export https_proxy=http://$cache_server:3128/
+export ftp_proxy=http://$cache_server:3128/
+export HTTP_PROXY=http://$cache_server:3128/
 #cache with devpi-server
 export PIP_INDEX_URL=http://127.0.0.1:3141/root/pypi/+simple/
 #cache with git-cache-http-server
@@ -15,9 +17,9 @@ export NPM_CONFIG_REGISTRY=http://127.0.0.1:4873/
 export YARN_REGISTRY=http://127.0.0.1:4873/
 #'
 fi
-timeout 1 bash -c 'cat < /dev/null > /dev/tcp/172.17.0.1/3632'
+timeout 1 bash -c 'cat < /dev/null > /dev/tcp/$cache_server/3632'
 if [ $? == 0 ] ; then
     echo "running distcc"
     export PATH=/usr/lib64/distcc:$PATH
-    export DISTCC_HOSTS='172.17.0.1'
+    export DISTCC_HOSTS="$cache_server"
 fi
