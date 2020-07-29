535718645634a151a4dc34bd1a9fe40459501a3e
Joseph C Wang <joequant@gmail.com>
2020-07-30 04:08:54 +0800

    comments on push image

--- bitstation/utils/push-image.sh
+++ bitstation/utils/push-image.sh
@@ -1,11 +1,15 @@
 #!/bin/bash -f
+# This script moves the latest podman image to docker.  This is because
+# the docker image is on a disk limited volume.
+
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+IMAGES=demo
 pushd $SCRIPT_DIR
-./containers.sh down
+./containers.sh down $IMAGES
 docker tag joequant/bitstation:production joequant/bitstation:latest
 docker tag joequant/nextcloud:production joequant/nextcloud:latest
 ./rm-stopped-containers.sh
 ./rm-untagged-images.sh
 skopeo copy containers-storage:localhost/joequant/bitstation  docker-daemon:joequant/bitstation:latest
-./containers.sh up
+./containers.sh up $IMAGES
 popd
