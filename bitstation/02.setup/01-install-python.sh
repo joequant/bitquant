120d55a25b538605431441c2e211f462de7402f3
Joseph C Wang <joequant@gmail.com>
2020-09-06 20:21:04 +0800

    don't replace metamanager

--- bitstation/02.setup/01-install-python.sh
+++ bitstation/02.setup/01-install-python.sh
@@ -588,10 +588,12 @@ jupyter serverextension enable --py jupyter_tensorboard --sys-prefix
 jupyter serverextension enable --py jupyterlab_code_formatter --sys-prefix
 
 #set up for jupyterfs
+: '
 cat <<EOF > /usr/etc/jupyter/jupyter_notebook_config.py
 # jupyterfs contents manager
 c.NotebookApp.contents_manager_class = "jupyterfs.metamanager.MetaManager"
 EOF
+'
 
 : '
 fabmanager create-admin --app superset
