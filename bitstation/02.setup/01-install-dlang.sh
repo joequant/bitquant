a7c935983ddee07bdb45477be5fb97cfcd5d25f6
Joseph C Wang <joequant@gmail.com>
2020-11-17 19:11:19 +0800

    add dlang_kernel

--- bitstation/02.setup/01-install-dlang.sh
+++ bitstation/02.setup/01-install-dlang.sh
@@ -4,3 +4,15 @@ set -v
 set +o errexit
 
 source $rootfsDir/tmp/proxy.sh
+pushd $rootfsDir/tmp
+git clone https://github.com/joequant/jupyter-wire.git/
+pushd jupyter-wire
+dub build
+pushd example/dlang_kernel
+dub build
+mv dlang_kernel $rootfsDir/usr/bin
+mkdir -p $rootfsDir/usr/share/jupyter/kernels/dlang_kernel
+cp kernel.json $rootfsDir/usr/share/jupyter/kernels/dlang_kernel
+popd
+popd
+popd
