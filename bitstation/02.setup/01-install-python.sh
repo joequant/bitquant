91b44412fa1727fed12bf206f0a435abd7335d28
Joseph C Wang <joequant@gmail.com>
2020-11-22 10:49:30 +0800

    requests

--- bitstation/02.setup/01-install-python.sh
+++ bitstation/02.setup/01-install-python.sh
@@ -41,6 +41,9 @@ pip3 install --upgrade pip --prefix /usr
 #remove to avoid attribute error
 pip3 uninstall numpy -y
 
+#needed to remove requests
+touch /usr/lib/python3.8/site-packages/requests-*.dist-info/RECORD
+
 pip3 install --upgrade numpy python-dateutil requests \
      matplotlib scipy --prefix /usr
 
