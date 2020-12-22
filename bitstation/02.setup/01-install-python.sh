3a5d054f7c7e654187f61041f6e041677535fea0
Joseph C Wang <joequant@gmail.com>
2020-12-22 23:54:48 +0800

    add gmaps

--- bitstation/02.setup/01-install-python.sh
+++ bitstation/02.setup/01-install-python.sh
@@ -100,7 +100,7 @@ pip3 install --upgrade $PYTHON_ARGS --prefix /usr --no-cache-dir \
 pip3 install --upgrade $PYTHON_ARGS --prefix /usr --no-cache-dir beakerx
 
 #pyspark
-
+npm install -g webpack
 parallel --halt 2 -j1 -n1 --linebuffer --tagstring '{}' "pip3 install --upgrade $PYTHON_ARGS --prefix /usr --no-cache-dir '{}'" ::: <<EOF
 nbformat
 ml-python
@@ -359,7 +359,6 @@ jupytext
 ipylab
 algorithmx
 ipydatetime
-gmaps
 ipycallback
 jupyterlab-latex
 jupyter_declarativewidgets
